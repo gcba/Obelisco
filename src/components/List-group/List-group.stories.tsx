00070963ae6c69e729df5979c8ae8bf58992613e
ghoyos95 <gonzalohoyos95@gmail.com>
2020-09-23 17:13:29 -0300

    fix

--- src/components/List-group/List-group.stories.tsx
+++ src/components/List-group/List-group.stories.tsx
@@ -1,13 +1,13 @@
 // Base
-import React from "react";
+import React from 'react';
 
 // Addons
-import { withA11y } from "@storybook/addon-a11y";
+import { withA11y } from '@storybook/addon-a11y';
 
 // Configuración del general del componente
 export default {
   // Usar | para separar entre secciones y / para generar carpetas
-  title: "Componentes|List",
+  title: 'Componentes|List',
   // Agregamos todos los plugins que usemos en las historias de este componente.
   decorators: [withA11y]
 };
@@ -27,8 +27,7 @@ export const Default = (): JSX.Element => (
         <h5 className="mb-1">Titulo</h5>
       </div>
       <p className="mb-1">
-        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget
-        risus varius blandit.
+        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
       </p>
     </a>
     <a href="#" className="list-group-item list-group-item-action">
@@ -36,8 +35,7 @@ export const Default = (): JSX.Element => (
         <h5 className="mb-1">Titulo</h5>
       </div>
       <p className="mb-1">
-        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget
-        risus varius blandit.
+        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
       </p>
     </a>
     <a href="#" className="list-group-item list-group-item-action">
@@ -45,8 +43,7 @@ export const Default = (): JSX.Element => (
         <h5 className="mb-1">Titulo</h5>
       </div>
       <p className="mb-1">
-        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget
-        risus varius blandit.
+        Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
       </p>
     </a>
   </div>
@@ -56,49 +53,37 @@ export const DefaultImage = (): JSX.Element => (
   <div className="list-group image">
     <a href="#" className="list-group-item list-group-item-action d-flex">
       <div className="col-2">
-        <img
-          className="img-list"
-          src="https://pbs.twimg.com/profile_images/1153697462546292736/O5jipnTv.jpg"
-        />
+        <img className="img-list" src="https://pbs.twimg.com/profile_images/1153697462546292736/O5jipnTv.jpg" />
       </div>
       <div className="col-10 justify-content-between">
         <h5 className="mb-1">Titulo</h5>
         <p className="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
-          eget risus varius blandit. Donec id elit non mi porta gravida at eget
-          metus.
+          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit
+          non mi porta gravida at eget metus.
         </p>
       </div>
     </a>
     <a href="#" className="list-group-item list-group-item-action d-flex">
       <div className="col-2">
-        <img
-          className="img-list"
-          src="https://pbs.twimg.com/profile_images/1153697462546292736/O5jipnTv.jpg"
-        />
+        <img className="img-list" src="https://pbs.twimg.com/profile_images/1153697462546292736/O5jipnTv.jpg" />
       </div>
       <div className="col-10 justify-content-between">
         <h5 className="mb-1">Titulo</h5>
         <p className="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
-          eget risus varius blandit. Donec id elit non mi porta gravida at eget
-          metus.
+          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit
+          non mi porta gravida at eget metus.
         </p>
       </div>
     </a>
     <a href="#" className="list-group-item list-group-item-action d-flex">
       <div className="col-2">
-        <img
-          className="img-list"
-          src="https://pbs.twimg.com/profile_images/1153697462546292736/O5jipnTv.jpg"
-        />
+        <img className="img-list" src="https://pbs.twimg.com/profile_images/1153697462546292736/O5jipnTv.jpg" />
       </div>
       <div className="col-10 justify-content-between">
         <h5 className="mb-1">Titulo</h5>
         <p className="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
-          eget risus varius blandit. Donec id elit non mi porta gravida at eget
-          metus.
+          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit
+          non mi porta gravida at eget metus.
         </p>
       </div>
     </a>
@@ -107,15 +92,8 @@ export const DefaultImage = (): JSX.Element => (
 
 export const TittleList = (): JSX.Element => (
   <div className="list-group tittle-list">
-    <h5 className="mb-1 list-group-item">
-      Nuevo protocolo de apertura para pymes y comercios
-    </h5>
-    <a
-      href="#"
-      className="list-group-item list-group-item-action"
-      data-toggle="collapse"
-      data-target="#collapseOne"
-    >
+    <h5 className="mb-1 list-group-item">Nuevo protocolo de apertura para pymes y comercios</h5>
+    <a href="#" className="list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseOne">
       Locales y comercios
     </a>
     <div id="collapseOne" className="collapse" data-parent="#accordion">
@@ -150,9 +128,7 @@ export const TittleList = (): JSX.Element => (
 
 export const SubList = (): JSX.Element => (
   <div className="list-group sub-list">
-    <h5 className="mb-1 list-group-item">
-      Nuevo protocolo de apertura para pymes y comercios
-    </h5>
+    <h5 className="mb-1 list-group-item">Nuevo protocolo de apertura para pymes y comercios</h5>
     <h6 className="mb-1 list-group-item">Locales y comercios</h6>
     <a href="#" className="list-group-item list-group-item-action">
       Local indumentaria y calzado. (Actividades comerciales de venta)
