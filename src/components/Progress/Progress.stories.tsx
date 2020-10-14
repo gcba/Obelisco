d2f976c0e68ecff252d9a7ceb81a53719fd0875b
lautarorodriguez96 <lar1996.vs@gmail.com>
2020-10-14 19:59:01 -0300

    primer maqueta de stepper

--- src/components/Progress/Progress.stories.tsx
+++ src/components/Progress/Progress.stories.tsx
@@ -13,11 +13,38 @@ export default {
   decorators: [withA11y]
 };
 
-export const Default = (): JSX.Element => {
+export const Stepper = (): JSX.Element => {
   return (
     <div className="container">
-      <div className="progress">
-        <div className="progress-bar style" role="progressbar" aria-value-now="25" aria-value-min="0" aria-value-max="100">25%</div>
+      <div className="row">
+        <div className="col-md-12">
+          <ul className="stepper stepper-horizontal">
+            <li className="completed">
+              <a href="#!">
+                <span className="circle">1</span>
+                <span className="label">Primer paso</span>
+              </a>
+            </li>
+            <li className="completed">
+              <a href="#!">
+                <span className="circle">2</span>
+                <span className="label">Segundo paso</span>
+              </a>
+            </li>
+            <li className="active">
+              <a href="#!">
+                <span className="circle">3</span>
+                <span className="label">Tercer paso</span>
+              </a>
+            </li>
+            <li className="active">
+              <a href="#!">
+                <span className="circle">4</span>
+                <span className="label">Cuarto paso</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
