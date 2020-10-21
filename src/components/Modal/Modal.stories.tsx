56189427980371d1a07229bd02a97975daf48e58
Martín <martin.techlowiec@gmail.com>
2020-10-21 12:18:38 -0300

    Agrego etiqueta <label>, corrección de estilos
    Cambio atributo "aria-label" por etiqueta <label> en el modal de formulario  y elimino la historia "Mucho texto"

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -74,43 +74,6 @@ export const Demo = (): JSX.Element => {
 
 Demo.story = { name: "Demo" };
 
-/*
-export const MuchoTexto = (): JSX.Element => {
-  return (
-  <div>
-    <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#anotherexampleModal">
-      Ver modal
-    </button>
-
-    <div className="modal fade" id="anotherexampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4 className="modal-title">Modal</h4>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet sagittis tortor. Nullam vel dolor elementum, consequat ex id, fringilla nulla. Sed est metus, porttitor quis dolor et, rutrum finibus lectus. Aliquam vel mollis justo, sit amet tincidunt mauris. Sed a dapibus urna, vel eleifend lacus. Ut sed efficitur velit, ut commodo libero. Fusce ullamcorper orci laoreet ex venenatis, id dapibus dui tristique. Pellentesque vel justo feugiat, ullamcorper augue vitae, sagittis ante. Donec id metus sed neque accumsan auctor. Etiam nec dolor turpis. Proin a risus sed justo porta venenatis posuere et ex. Ut finibus mi ac bibendum ultricies. Sed bibendum eu felis at consequat. Sed blandit risus blandit tellus aliquam placerat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-
-            <p>Fusce nec ultricies mauris. Nam molestie hendrerit magna, at consequat mi rutrum at. Proin dolor arcu, dictum vitae felis eu, vestibulum viverra nisi. Maecenas non tincidunt velit, vel eleifend nulla. Duis vel molestie nibh. Suspendisse accumsan venenatis felis et vulputate. Aenean posuere auctor elit a dignissim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin maximus lorem quis odio maximus, sed venenatis dolor sodales. Ut in augue sed nisl tincidunt vestibulum. Nulla facilisi.</p>
-
-            <p>Praesent sit amet leo sed magna finibus pellentesque. Praesent tristique lorem sed vulputate bibendum. Etiam aliquam augue nulla, a vestibulum metus fringilla sed. Sed mattis fermentum enim nec gravida. Duis consectetur vel massa sed venenatis. Praesent pellentesque massa quis nibh ornare, in sagittis dui maximus. Vivamus id dolor et elit pulvinar eleifend eget sed nibh. Donec quis lorem leo. Maecenas et libero ultricies, gravida justo in, tincidunt nunc. Donec pharetra feugiat purus ut varius.</p>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary btn-sm" data-dismiss="modal">Acción 2</button>
-            <button type="button" className="btn btn-primary btn-sm">Acción 1</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  );
-};
-
-MuchoTexto.story = { name: "Mucho Texto" };
-*/
 
 export const Formulario = (): JSX.Element => {
   return (
@@ -130,9 +93,11 @@ export const Formulario = (): JSX.Element => {
           </div>
           <div className="modal-body">
             <form className="form-group">
-              <input type="text" className="form-control" id="name-input" name="name" placeholder="Asunto" aria-label="Ingrese el asunto"/>
+              <label htmlFor="name-input">Campo simple</label>
+              <input type="text" className="form-control" id="name-input" name="name" placeholder="Ejemplo: Camila" />
 
-              <textarea className="form-control" id="text-area" placeholder="Mensaje" aria-label="Ingrese el mensaje" rows={3}></textarea>
+              <label htmlFor="text-area">Bloque de texto</label>
+              <textarea className="form-control" id="text-area" placeholder="Texto" maxlength="100" rows={3}></textarea>
             </form>
           </div>
           <div className="modal-footer">
