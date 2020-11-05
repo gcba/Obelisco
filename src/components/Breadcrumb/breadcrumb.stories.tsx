42615fa95b2c6dde91e3390b85a930b009615690
Nicolás Rougier <nico@innovid.com>
2020-11-05 02:16:52 -0300

    Ajustes en el breadcrumb

--- src/components/Breadcrumb/breadcrumb.stories.tsx
+++ src/components/Breadcrumb/breadcrumb.stories.tsx
@@ -1,32 +1,20 @@
 // Base
-import React from "react";
+import React from 'react';
 
 // Addons
-import { withA11y } from "@storybook/addon-a11y";
+import { withA11y } from '@storybook/addon-a11y';
 
 // Configuración del general del componente
 export default {
-  // Usar | para separar entre secciones y / para generar carpetas
-  title: "Componentes|Breadcrumb",
-  // Vinculamos el componente de react que estamos usando. Si no existe ninguno eliminar el campo.
-
-  // Agregamos todos los plugins que usemos en las historias de este componente.
+  title: 'Componentes|Breadcrumb',
   decorators: [withA11y]
 };
 
-/*
- * Para cada historia vamos a exportar una constrante que devuelva un element JSX con el html.
- * Más información en https://storybook.js.org/docs/basics/writing-stories/
- *
- * No hace falta que las historias usen componentes de react.
- * pueden devolver solamente el html estático necesario
- */
-
-export const Breadcrumb = (): JSX.Element => (
+export const UnNivel = (): JSX.Element => (
   <div>
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
-        <li className="breadcrumb-item active bread-first" aria-current="page">
+        <li className="breadcrumb-item">
           <a href="#">Buenos Aires</a>
         </li>
       </ol>
@@ -34,31 +22,31 @@ export const Breadcrumb = (): JSX.Element => (
   </div>
 );
 
-export const BreadcrumbAnterior = (): JSX.Element => (
+export const DosNiveles = (): JSX.Element => (
   <div>
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
-        <li className="breadcrumb-item bread-first">
+        <li className="breadcrumb-item">
           <a href="#">Buenos Aires</a>
         </li>
-        <li className="breadcrumb-item active" aria-current="page">
+        <li className="breadcrumb-item">
           <a href="#">Pagina anterior</a>
         </li>
       </ol>
     </nav>
   </div>
 );
-export const BreadcrumbInterm = (): JSX.Element => (
+export const TresNiveles = (): JSX.Element => (
   <div>
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
-        <li className="breadcrumb-item bread-first">
+        <li className="breadcrumb-item">
           <a href="#">Buenos Aires</a>
         </li>
         <li className="breadcrumb-item">
           <a href="#">Intermedia</a>
         </li>
-        <li className="breadcrumb-item active" aria-current="page">
+        <li className="breadcrumb-item">
           <a href="#">Pagina anterior</a>
         </li>
       </ol>
@@ -66,20 +54,20 @@ export const BreadcrumbInterm = (): JSX.Element => (
   </div>
 );
 
-export const BreadcrumbInterme2 = (): JSX.Element => (
+export const CuatroNiveles = (): JSX.Element => (
   <div>
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
-        <li className="breadcrumb-item bread-first">
+        <li className="breadcrumb-item">
           <a href="#">Buenos Aires</a>
         </li>
         <li className="breadcrumb-item">
           <a href="#">Intermedia</a>
         </li>
-        <li className="breadcrumb-item" aria-current="page">
+        <li className="breadcrumb-item">
           <a href="#">Intermedia</a>
         </li>
-        <li className="breadcrumb-item active" aria-current="page">
+        <li className="breadcrumb-item">
           <a href="#">Pagina anterior</a>
         </li>
       </ol>
