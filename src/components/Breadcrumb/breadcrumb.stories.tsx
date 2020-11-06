70d405f893151037d37c2f9e245b4845be945dc5
Gonzalo Hoyos <32983207+ghoyos95@users.noreply.github.com>
2020-11-06 00:56:37 -0300

    feat: Agrega el componente breadcrumb

---
+++ src/components/Breadcrumb/breadcrumb.stories.tsx
@@ -0,0 +1,76 @@
+// Base
+import React from 'react';
+
+// Addons
+import { withA11y } from '@storybook/addon-a11y';
+
+// Configuración del general del componente
+export default {
+  title: 'Componentes|Breadcrumb',
+  decorators: [withA11y]
+};
+
+export const UnNivel = (): JSX.Element => (
+  <div>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <a href="#">Buenos Aires</a>
+        </li>
+      </ol>
+    </nav>
+  </div>
+);
+
+export const DosNiveles = (): JSX.Element => (
+  <div>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <a href="#">Buenos Aires</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Pagina anterior</a>
+        </li>
+      </ol>
+    </nav>
+  </div>
+);
+export const TresNiveles = (): JSX.Element => (
+  <div>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <a href="#">Buenos Aires</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Intermedia</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Pagina anterior</a>
+        </li>
+      </ol>
+    </nav>
+  </div>
+);
+
+export const CuatroNiveles = (): JSX.Element => (
+  <div>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <a href="#">Buenos Aires</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Intermedia</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Intermedia</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Pagina anterior</a>
+        </li>
+      </ol>
+    </nav>
+  </div>
+);
