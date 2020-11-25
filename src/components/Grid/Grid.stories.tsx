1614acde10dc012a67fe52106e86c208b8b76c70
Nicolás Rougier <nico@innovid.com>
2020-11-25 02:20:09 -0300

    fix: Agrega la card horizontal y con íconos

--- src/components/Grid/Grid.stories.tsx
+++ src/components/Grid/Grid.stories.tsx
@@ -63,43 +63,45 @@ Tamanos.story = { name: 'Tamaños' };*/
 
 export const Columnas = (): JSX.Element => {
   return (
-    <div className="container text-center">
-      <div className="row">
-        <div className="col-1">
-          <div>1</div>
-        </div>
-        <div className="col-1">
-          <div>2</div>
-        </div>
-        <div className="col-1">
-          <div>3</div>
-        </div>
-        <div className="col-1">
-          <div>4</div>
-        </div>
-        <div className="col-1">
-          <div>5</div>
-        </div>
-        <div className="col-1">
-          <div>6</div>
-        </div>
-        <div className="col-1">
-          <div>7</div>
-        </div>
-        <div className="col-1">
-          <div>8</div>
-        </div>
-        <div className="col-1">
-          <div>9</div>
-        </div>
-        <div className="col-1">
-          <div>10</div>
-        </div>
-        <div className="col-1">
-          <div>11</div>
-        </div>
-        <div className="col-1">
-          <div>12</div>
+    <div className="grid-wraper">
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-1">
+            <div>1</div>
+          </div>
+          <div className="col-1">
+            <div>2</div>
+          </div>
+          <div className="col-1">
+            <div>3</div>
+          </div>
+          <div className="col-1">
+            <div>4</div>
+          </div>
+          <div className="col-1">
+            <div>5</div>
+          </div>
+          <div className="col-1">
+            <div>6</div>
+          </div>
+          <div className="col-1">
+            <div>7</div>
+          </div>
+          <div className="col-1">
+            <div>8</div>
+          </div>
+          <div className="col-1">
+            <div>9</div>
+          </div>
+          <div className="col-1">
+            <div>10</div>
+          </div>
+          <div className="col-1">
+            <div>11</div>
+          </div>
+          <div className="col-1">
+            <div>12</div>
+          </div>
         </div>
       </div>
     </div>
@@ -108,59 +110,61 @@ export const Columnas = (): JSX.Element => {
 
 export const Divisiones = (): JSX.Element => {
   return (
-    <div className="container text-center">
-      <div className="row mb-2">
-        <div className="col-2">
-          <div>col-2</div>
+    <div className="grid-wraper">
+      <div className="container text-center">
+        <div className="row mb-2">
+          <div className="col-2">
+            <div>col-2</div>
+          </div>
+          <div className="col-6">
+            <div>col-6</div>
+          </div>
+          <div className="col-4">
+            <div>col-4</div>
+          </div>
         </div>
-        <div className="col-6">
-          <div>col-6</div>
+        <div className="row mb-2">
+          <div className="col-5">
+            <div>col-5</div>
+          </div>
+          <div className="col-7">
+            <div>col-7</div>
+          </div>
         </div>
-        <div className="col-4">
-          <div>col-4</div>
+        <div className="row mb-2">
+          <div className="col-4">
+            <div>col-4</div>
+          </div>
+          <div className="col-8">
+            <div>col-8</div>
+          </div>
         </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-5">
-          <div>col-5</div>
+        <div className="row mb-2">
+          <div className="col-9">
+            <div>col-9</div>
+          </div>
+          <div className="col-3">
+            <div>col-3</div>
+          </div>
         </div>
-        <div className="col-7">
-          <div>col-7</div>
+        <div className="row mb-2">
+          <div className="col-1">
+            <div>col-1</div>
+          </div>
+          <div className="col-10">
+            <div>col-10</div>
+          </div>
+          <div className="col-1">
+            <div>col-1</div>
+          </div>
         </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-4">
-          <div>col-4</div>
-        </div>
-        <div className="col-8">
-          <div>col-8</div>
-        </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-9">
-          <div>col-9</div>
-        </div>
-        <div className="col-3">
-          <div>col-3</div>
-        </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-1">
-          <div>col-1</div>
-        </div>
-        <div className="col-10">
-          <div>col-10</div>
-        </div>
-        <div className="col-1">
-          <div>col-1</div>
-        </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-11">
-          <div>col-11</div>
-        </div>
-        <div className="col-1">
-          <div>col-1</div>
+        <div className="row mb-2">
+          <div className="col-11">
+            <div>col-11</div>
+          </div>
+          <div className="col-1">
+            <div>col-1</div>
+          </div>
         </div>
       </div>
     </div>
@@ -169,45 +173,47 @@ export const Divisiones = (): JSX.Element => {
 
 export const AlineacionHorizontal = (): JSX.Element => {
   return (
-    <div className="container">
-      <div className="row justify-content-start mb-2">
-        <div className="col-4">
-          <div>Una de dos columnas</div>
+    <div className="grid-wraper">
+      <div className="container">
+        <div className="row justify-content-start mb-2">
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
         </div>
-        <div className="col-4">
-          <div>Una de dos columnas</div>
+        <div className="row justify-content-center mb-2">
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
         </div>
-      </div>
-      <div className="row justify-content-center mb-2">
-        <div className="col-4">
-          <div>Una de dos columnas</div>
+        <div className="row justify-content-end mb-2">
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
         </div>
-        <div className="col-4">
-          <div>Una de dos columnas</div>
+        <div className="row justify-content-around mb-2">
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
         </div>
-      </div>
-      <div className="row justify-content-end mb-2">
-        <div className="col-4">
-          <div>Una de dos columnas</div>
-        </div>
-        <div className="col-4">
-          <div>Una de dos columnas</div>
-        </div>
-      </div>
-      <div className="row justify-content-around mb-2">
-        <div className="col-4">
-          <div>Una de dos columnas</div>
-        </div>
-        <div className="col-4">
-          <div>Una de dos columnas</div>
-        </div>
-      </div>
-      <div className="row justify-content-between mb-2">
-        <div className="col-4">
-          <div>Una de dos columnas</div>
-        </div>
-        <div className="col-4">
-          <div>Una de dos columnas</div>
+        <div className="row justify-content-between mb-2">
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
+          <div className="col-4">
+            <div>Una de dos columnas</div>
+          </div>
         </div>
       </div>
     </div>
@@ -218,27 +224,29 @@ AlineacionHorizontal.story = { name: 'Alineación Horizontal' };
 
 export const OrdenarClases = (): JSX.Element => {
   return (
-    <div className="container">
-      <div className="row mb-2">
-        <div className="col-4">
-          <div>Primero, pero desordenado</div>
+    <div className="grid-wraper">
+      <div className="container">
+        <div className="row mb-2">
+          <div className="col-4">
+            <div>Primero, pero desordenado</div>
+          </div>
+          <div className="col-4 order-12">
+            <div>Tercero, pero primero</div>
+          </div>
+          <div className="col-4 order-1">
+            <div>Segundo, pero último</div>
+          </div>
         </div>
-        <div className="col-4 order-12">
-          <div>Tercero, pero primero</div>
-        </div>
-        <div className="col-4 order-1">
-          <div>Segundo, pero último</div>
-        </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-4 order-last">
-          <div>Tercero, pero primero</div>
-        </div>
-        <div className="col-4">
-          <div>Segundo, pero desordenado</div>
-        </div>
-        <div className="col-4 order-first">
-          <div>Primero pero ultimo</div>
+        <div className="row mb-2">
+          <div className="col-4 order-last">
+            <div>Tercero, pero primero</div>
+          </div>
+          <div className="col-4">
+            <div>Segundo, pero desordenado</div>
+          </div>
+          <div className="col-4 order-first">
+            <div>Primero pero ultimo</div>
+          </div>
         </div>
       </div>
     </div>
@@ -247,42 +255,44 @@ export const OrdenarClases = (): JSX.Element => {
 
 export const Desplazamientos = (): JSX.Element => {
   return (
-    <div className="container">
-      <div className="row mb-2">
-        <div className="col-md-4">
-          <div>.col-md-4</div>
+    <div className="grid-wraper">
+      <div className="container">
+        <div className="row mb-2">
+          <div className="col-md-4">
+            <div>.col-md-4</div>
+          </div>
+          <div className="col-md-4 offset-md-4">
+            <div>.col-md-4 .offset-md-4</div>
+          </div>
         </div>
-        <div className="col-md-4 offset-md-4">
-          <div>.col-md-4 .offset-md-4</div>
+        <div className="row mb-2">
+          <div className="col-md-3 offset-md-3">
+            <div>.col-md-3 .offset-md-3</div>
+          </div>
+          <div className="col-md-3 offset-md-3">
+            <div>.col-md-3 .offset-md-3</div>
+          </div>
         </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-md-3 offset-md-3">
-          <div>.col-md-3 .offset-md-3</div>
+        <div className="row mb-2">
+          <div className="col-md-6 offset-md-3">
+            <div>.col-md-6 .offset-md-3</div>
+          </div>
         </div>
-        <div className="col-md-3 offset-md-3">
-          <div>.col-md-3 .offset-md-3</div>
+        <div className="row mb-2">
+          <div className="col-sm-5 col-md-6">
+            <div>.col-sm-5 .col-md-6</div>
+          </div>
+          <div className="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+            <div>.col-sm-5 .offset-sm-2 .col-md-6 .offset-md-0</div>
+          </div>
         </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-md-6 offset-md-3">
-          <div>.col-md-6 .offset-md-3</div>
-        </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-sm-5 col-md-6">
-          <div>.col-sm-5 .col-md-6</div>
-        </div>
-        <div className="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
-          <div>.col-sm-5 .offset-sm-2 .col-md-6 .offset-md-0</div>
-        </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col-sm-6 col-md-5 col-lg-6">
-          <div>.col-sm-6 .col-md-5 .col-lg-6</div>
-        </div>
-        <div className="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
-          <div>.col-sm-6 .col-md-5 .offset-md-2 .col-lg-6 .offset-lg-0</div>
+        <div className="row mb-2">
+          <div className="col-sm-6 col-md-5 col-lg-6">
+            <div>.col-sm-6 .col-md-5 .col-lg-6</div>
+          </div>
+          <div className="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
+            <div>.col-sm-6 .col-md-5 .offset-md-2 .col-lg-6 .offset-lg-0</div>
+          </div>
         </div>
       </div>
     </div>
