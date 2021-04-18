0125774a284b0cdf3400094e4de459308eff5045
Nicolás Rougier <nico@innovid.com>
2021-04-18 13:00:13 -0300

    docs: Ajustes en la historia de tarjetas sin fondo

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -38,7 +38,7 @@ export const Ícono = (): JSX.Element => {
     <div className="cards-container">
       <a className="card" href="#">
         <div className="card-body">
-          <i className="bx bx-news card-icon text-info"></i>
+          <i className="bx bx-news card-icon text-info" />
           <h4 className="card-title">Título de tarjeta</h4>
           <p className="card-text">
             Recomendamos en este espacio no utilizar más de 3 líneas, para generar una lectura óptima.
@@ -89,13 +89,12 @@ export const TitleTextLink = (): JSX.Element => {
     <div className="cards-container">
       <div className="card card-simple">
         <div className="card-body">
-          <h4 className="card-title">Título Tarjeta</h4>
+          <h4 className="card-title">
+            <a href="#">Título Tarjeta</a>
+          </h4>
           <p className="card-text">
             Recomendamos en este espacio no utilizar más de 3 líneas, para generar una lectura óptima.
           </p>
-          <a href="#" className="link-arrow">
-            Ver título card
-          </a>
         </div>
       </div>
     </div>
@@ -103,47 +102,21 @@ export const TitleTextLink = (): JSX.Element => {
 };
 TitleTextLink.story = { name: 'Sin contenedor' };
 
-//Proximas Cards
-/*export const OverlineTitleTextButtonCaption = (): JSX.Element => {
+export const TitleIconTextLink = (): JSX.Element => {
   return (
-    <div className="">
-      <div className="card">        
+    <div className="cards-container">
+      <div className="card card-simple">
         <div className="card-body">
-          <div className="card-overline">OVERLINE</div>
-          <h4 className="card-title">Título Card </h4>
+          <i className="bx bx-news card-icon text-success" />
+          <h4 className="card-title">
+            <a href="#">Título Tarjeta</a>
+          </h4>
           <p className="card-text">
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy
-            eirmod tempor
-          </p>   
-          <hr className="line"></hr> 
-          <div className="subcontent"> 
-            <button type="button" className="btn btn-primary btn-sm ml-0">Botón</button>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </div>   
+            Recomendamos en este espacio no utilizar más de 3 líneas, para generar una lectura óptima.
+          </p>
         </div>
       </div>
     </div>
   );
 };
-OverlineTitleTextButtonCaption.story = { name: 'Overline + Título + Texto + Botón + Caption'};*/
-
-/*
-export const EstaticAction = (): JSX.Element => {
-  return (
-      <div className="card">
-        <div className="card-body">
-          <h4 className="card-title">Jefatura de Gobierno</h4>
-          <div className="card-subtitle mb-2">Ejes principales</div>
-          <a href="#" className="card-link-btn">Jefe de Gobierno</a>
-          <a href="#" className="card-link-btn">Vicejefatura</a>
-          <a href="#" className="card-link-btn">Compromisos</a>
-          <a href="#" className="card-link-btn">BA Elige</a>
-          <hr className="line"></hr>
-          <div className="card-subtitle mb-2">Horacio Rodriguez Larreta</div>
-          <span className="text-muted">Jefe de Gobierno</span>
-        </div>
-      </div>
-  );
-};
-EstaticAction.story = { name: 'Estáticas + contenido accionable'};*/
+TitleIconTextLink.story = { name: 'Sin contenedor con ícono' };
