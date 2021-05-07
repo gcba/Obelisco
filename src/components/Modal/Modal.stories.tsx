e192fca1c057d649f07648721ed4333b7d9bb99c
Nicolás Rougier <nico@innovid.com>
2021-05-06 21:31:02 -0300

    Ajustes en modals

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -14,26 +14,29 @@ export default {
 
 export const Generico = (): JSX.Element => {
   return (
-    <div className="modal d-block" tabIndex={-1} role="dialog">
-      <div className="modal-dialog modal-sm" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4 className="modal-title">Modal</h4>
-          </div>
-          <div className="modal-body">
-            <p>Esta es la descripción del modal.</p>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-link btn-sm" data-dismiss="modal">
-              Acción 2
-            </button>
-            <button type="button" className="btn btn-danger btn-sm">
-              Acción 1
-            </button>
+    <>
+      <div className="modal d-block" tabIndex={-1} role="dialog">
+        <div className="modal-dialog modal-sm" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Modal</h4>
+            </div>
+            <div className="modal-body">
+              <p>Esta es la descripción del modal.</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-link btn-sm" data-dismiss="modal">
+                Acción 2
+              </button>
+              <button type="button" className="btn btn-danger btn-sm">
+                Acción 1
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="modal-backdrop show"></div>
+    </>
   );
 };
 
