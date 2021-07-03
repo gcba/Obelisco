729a2ab85f6bfcc4e93bee3c2e8d1c19bf5f4d0f
Nicolás Rougier <nico@innovid.com>
2021-07-03 11:58:46 -0300

    fix: Exporta el componente de paginación

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /*
  * Por el momento no estamos dando soporte para componentes de react.
- * Los componentes presentes en este proyecto son de uso exclusivo para la documetanción y no deben ser utilizados en proyectos reales.
+ * Los componentes presentes en esta librería son de uso exclusivo para la documetanción y no deben ser utilizados en proyectos reales.
  */
 
 // Components
-export { Button } from './components/Button';
+export { Button, ButtonProps } from './components/Button';
 export { ButtonType } from './components/utils';
-
 export { Card, SimpleCard, SimpleCardProps, CardProps } from './components/Card';
+export { Pagination, PaginationProps } from './components/Pagination';
