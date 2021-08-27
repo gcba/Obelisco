1e6eb8537fe8f59598e9e09cdbe954bf8dff66e9
Lautaro Rodriguez <lar1996.vs@gmail.com>
2021-08-26 21:33:23 -0300

    correciones git y falta de icono en input

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -5,8 +5,8 @@ import './Header.stories.scss';
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
 
-
 // Components
+import { iconTypes } from '../utils';
 
 // Config
 export default {
@@ -25,15 +25,13 @@ export const Header = (): JSX.Element => {
             </a>
           </div>
           <div className="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
-            <div className="form-wrapper">
-              <div className="form-group">
-                <input type="search" className="form-control icon-search" id="name-input" name="name" placeholder="Buscar..." />
+            {iconTypes.map(type => (
+              <div className="form-wrapper">
+                <div className="form-group">
+                  <input type="search" className={`form-control form-${type}`} id="name-input" name="name" placeholder="Buscar..." />
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="col-md-2 col-sm-12 col-xs-12 d-flex justify-content-center p-2">
-            <span>Mi perfil</span>
-            <span className="icon-user"></span>
+            ))}
           </div>
         </div>
       </div>
@@ -41,3 +39,7 @@ export const Header = (): JSX.Element => {
   );
 };
 
+{/* <div className="col-md-2 col-sm-12 col-xs-12 d-flex justify-content-center p-2">
+            <span>Mi perfil</span>
+            <span className="icon-user"></span>
+          </div> */}
