3ac10a109cbeb11d34759bc6fbb8eb961d89e443
lautarorodriguez96 <70086268+lautarorodriguez96@users.noreply.github.com>
2021-11-08 15:30:48 -0300

    fix: feat enlace
    * componente enlace
    
    * cambios mencionados en github
    
    * Ajustes en los colores
    
    Co-authored-by: Nicolás Rougier <nico@innovid.com>

---
+++ src/components/Link/Link.stories.tsx
@@ -0,0 +1,42 @@
+// Base
+import React from 'react';
+import './Link.stories.scss';
+
+// Addons
+import { withA11y } from '@storybook/addon-a11y';
+
+// Components
+
+// Config
+export default {
+  title: 'Componentes|Enlace',
+  decorators: [withA11y]
+};
+
+export const Default = (): JSX.Element => {
+  return (
+    <p>
+      You never see a frog so modest and{' '}
+      <a className="" href="#" target="_blank">
+        enlace en párrafo
+      </a>{' '}
+      as he was, for all he was so gifted. And when it come to fair and square jumping on a dead level, he could get
+      over more ground at one straddle than any animal of his breed you ever see.
+    </p>
+  );
+};
+
+export const Externo = (): JSX.Element => {
+  return (
+    <div>
+      <p>
+        You never see a frog so modest and{' '}
+        <a className="external" href="#" target="_blank">
+          enlace externo
+        </a>{' '}
+        as he was, for all he was so gifted. And when it come to fair and square jumping on a dead level, he could get
+        over more ground at one straddle than any animal of his breed you ever see.
+      </p>
+    </div>
+  );
+};
