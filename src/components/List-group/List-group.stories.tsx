dcdc94707a532df035c3f70addfc1648348ddd73
Alan199912 <alan199912@gmail.com>
2022-02-24 12:17:46 -0300

    refactor accesos

--- src/components/List-group/List-group.stories.tsx
+++ src/components/List-group/List-group.stories.tsx
@@ -5,6 +5,11 @@ import React from 'react';
 import { withA11y } from '@storybook/addon-a11y';
 
 import './List-group.stories.scss';
+import { BigAccess } from './BigAccess.stories';
+import { ModeOfUseAccess } from './ModeOfUseAccess.stories';
+import { SmallAccess } from './SmallAcces.stories';
+import { ModeOfUseTypeTwo } from './ModeOfUseAccessTwo.stories';
+import { AccessMobile } from './AccessMobile.stories';
 
 // Configuración del general del componente
 export default {
@@ -12,71 +17,41 @@ export default {
   decorators: [withA11y]
 };
 
-export const Acceso = (): JSX.Element => (
-  <div className="list-group-container">
-    <div className="list-group">
-      <a href="#" className="list-group-item">
-        Datos de mi cuenta
-      </a>
-      <a href="#" className="list-group-item hover">
-        Opciones de seguridad
-      </a>
-      <a href="#" className="list-group-item focus">
-        Datos de mi cuenta
-      </a>
-      <a href="#" className="list-group-item hover focus">
-        Opciones de seguridad
-      </a>
-      <a href="#" className="list-group-item list-group-item-logout">
-        Cerrar sesión
-      </a>
-    </div>
+export const AccesoGrande = (): JSX.Element => (
+  <div>
+    <h2>Acceso grande</h2>
+    <BigAccess />
   </div>
 );
 
-export const AccesoPequeno = (): JSX.Element => (
-  <div className="list-group-container">
-    <div className="list-group list-group-sm">
-      <a href="#" className="list-group-item">
-        Datos de mi cuenta
-      </a>
-      <a href="#" className="list-group-item">
-        Opciones de seguridad
-      </a>
-      <a href="#" className="list-group-item list-group-item-logout">
-        Cerrar sesión
-      </a>
+export const ModoDeUso = (): JSX.Element => {
+  return (
+    <div>
+      <ModeOfUseAccess />
     </div>
-  </div>
-);
-AccesoPequeno.story = { name: 'Acceso Pequeño' };
+  );
+};
 
-export const Categorias = (): JSX.Element => (
-  <div className="list-group-container">
-    <div className="list-group list-group-sm">
-      <h4 className="list-group-title">Seleccioná un trámite</h4>
-      <p className="list-group-category">Categoría 1</p>
-      <a href="#" className="list-group-item">
-        Opción 1
-      </a>
-      <a href="#" className="list-group-item">
-        Opción 2
-      </a>
-      <a href="#" className="list-group-item">
-        Opción 3
-      </a>
-
-      <p className="list-group-category">Categoría 2</p>
-      <a href="#" className="list-group-item">
-        Opción 1
-      </a>
-      <a href="#" className="list-group-item">
-        Opción 2
-      </a>
-      <a href="#" className="list-group-item">
-        Opción 3
-      </a>
+export const AccesoChico = (): JSX.Element => {
+  return (
+    <div>
+      <SmallAccess />
     </div>
-  </div>
-);
-Categorias.story = { name: 'Categorías' };
+  );
+};
+
+export const ModoDeUso2 = (): JSX.Element => {
+  return (
+    <div>
+      <ModeOfUseTypeTwo />
+    </div>
+  );
+};
+
+export const Mobile = (): JSX.Element => {
+  return (
+    <div>
+      <AccessMobile />
+    </div>
+  );
+};
