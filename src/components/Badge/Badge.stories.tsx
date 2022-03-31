e8a14175722b3db5167b988d633ce982969ba62c
lautarorodriguez96 <70086268+lautarorodriguez96@users.noreply.github.com>
2022-03-31 20:52:23 -0300

    Nuevas etiquetas (#56)
    * cambios en etiquetas
    
    * colores en etiquetas
    
    * cambio de variables en colores
    
    * falta el color del secondary
    
    * correcciones etiquetas
    
    * css etiquetas
    
    * nuevas correcciones
    
    * feat: componente etiqueta
    
    * feat componente etiqueta
    
    * feat: Nuevas Etiquetas

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -14,21 +14,31 @@ export default {
   component: Badge
 };
 
-export const Simple = (): JSX.Element => <Badge text="Etiqueta 1" />;
+export const Simple = (): JSX.Element => <Badge text="Etiqueta 1" className="badge-secondary" />;
 
 export const Multiple = (): JSX.Element => (
   <>
-    <Badge text="Etiqueta 1" />
-    <Badge text="Etiqueta 2" />
-    <Badge text="Etiqueta 3" />
+    <Badge text="Etiqueta 1" className="badge-secondary" />
+    <Badge text="Etiqueta 2" className="badge-secondary" />
+    <Badge text="Etiqueta 3" className="badge-secondary" />
+  </>
+);
+
+export const Colores = (): JSX.Element => (
+  <>
+    <Badge text="Etiqueta 1" className="badge-secondary" />
+    <Badge text="Etiqueta 2" className="badge-danger" />
+    <Badge text="Etiqueta 3" className="badge-success" />
+    <Badge text="Etiqueta 4" className="badge-info" />
+    <Badge text="Etiqueta 5" className="badge-primary" />
   </>
 );
 
 export const Link = (): JSX.Element => (
   <>
-    <Badge text="Etiqueta 1" href="#" />
-    <Badge text="Etiqueta 2" href="#" className="active" />
-    <Badge text="Etiqueta 3" href="#" className="focus active" />
-    <Badge text="Etiqueta 4" href="#" className="focus" />
+    <Badge text="Etiqueta 1" href="#" className="badge-secondary" />
+    <Badge text="Etiqueta 2" href="#" className="hover badge-secondary" />
+    <Badge text="Etiqueta 3" href="#" className="focus hover badge-secondary" />
+    <Badge text="Etiqueta 4" href="#" className="focus badge-secondary" />
   </>
 );
