214341a77bcd9f2601265bb33d31a0e3ed086267
Martín Té <51392946+techlowiec@users.noreply.github.com>
2022-04-01 09:28:03 -0300

    feat: Actualización del componente navegación (#52)
    * feat: Actualización del componente navegación
    
    * feat: Se agregaron nav horizontales con borde, icono y sus tamaños
    
    * feat: Se agrego icono personalizable
    
    * feat: corrección de pesos tipográficos y cambios de valores a REM
    
    * feat: Correcion  de hover en active-child, focus & active
    
    Co-authored-by: Alan199912 <alan199912@gmail.com>

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -14,12 +14,14 @@ export interface NavProps {
   selected?: string;
   navSize?: 'default' | 'large';
   onClick?: (id: string) => void;
+  hasBordered?: boolean;
+  hasIcon?: boolean;
 }
 
 const listClasses = 'nav flex-column nav-pills';
 
 export const Nav: React.FC<NavProps> = (props: React.PropsWithChildren<NavProps>) => {
-  const { items, selected, navSize, onClick } = props;
+  const { items, selected, navSize, onClick, hasIcon } = props;
 
   const mainListClasses = classnames(listClasses, {
     'nav-lg': navSize === 'large'
@@ -29,7 +31,34 @@ export const Nav: React.FC<NavProps> = (props: React.PropsWithChildren<NavProps>
     <nav>
       <ul className={mainListClasses}>
         {items.map((item) => (
-          <NavItemComponent {...item} key={item.id} level={0} onClick={onClick} selected={selected} />
+          <NavItemComponent {...item} key={item.id} level={0} onClick={onClick} selected={selected} hasIcon={hasIcon} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const listClassesHorizontal = 'nav flex-row nav-pills';
+
+export const NavHorizontal: React.FC<NavProps> = (props: React.PropsWithChildren<NavProps>) => {
+  const { items, selected, navSize, onClick, hasIcon } = props;
+
+  const mainListClasses = classnames(listClassesHorizontal, {
+    'nav-lg': navSize === 'large'
+  });
+
+  return (
+    <nav>
+      <ul className={mainListClasses}>
+        {items.map((item) => (
+          <NavItemComponentHorizontal
+            {...item}
+            key={item.id}
+            level={0}
+            onClick={onClick}
+            selected={selected}
+            hasIcon={hasIcon}
+          />
         ))}
       </ul>
     </nav>
@@ -40,10 +69,13 @@ interface NavItemComponentProps extends NavItem {
   level: number;
   selected?: string;
   onClick?: (id: string) => void;
+  hasBordered?: boolean;
+  isSize?: boolean;
+  hasIcon?: boolean;
 }
 
 const NavItemComponent: React.FC<NavItemComponentProps> = (props: NavItemComponentProps) => {
-  const { name, id, href, children, level, disabled, selected } = props;
+  const { name, id, href, children, level, disabled, selected, hasIcon } = props;
 
   const isActive = selected && id === selected;
   const hasActiveChild = checkActiveChild(children, selected);
@@ -69,12 +101,82 @@ const NavItemComponent: React.FC<NavItemComponentProps> = (props: NavItemCompone
         onClick={handleClick}
         aria-disabled={disabled}
         tabIndex={disabled ? -1 : undefined}>
-        {name}
+        {hasIcon ? (
+          <div className="nav-icon">
+            <i className="bx bxs-user-circle"></i>
+            <span>{name}</span>
+          </div>
+        ) : (
+          <span>{name}</span>
+        )}
       </a>
       {!disabled && (isActive || hasActiveChild) && children && (
         <ul className={listClasses}>
           {children.map((item) => (
-            <NavItemComponent {...item} key={item.id} level={level + 1} onClick={props.onClick} selected={selected} />
+            <NavItemComponent
+              {...item}
+              key={item.id}
+              level={level + 1}
+              onClick={props.onClick}
+              selected={selected}
+              hasIcon={hasIcon}
+            />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+};
+
+const NavItemComponentHorizontal: React.FC<NavItemComponentProps> = (props: NavItemComponentProps) => {
+  const { name, id, href, children, level, disabled, selected, hasBordered, isSize, hasIcon } = props;
+
+  const isActive = selected && id === selected;
+  const hasActiveChild = checkActiveChild(children, selected);
+
+  const linkClassName = classnames('nav-link', {
+    'nav-link-sm': isSize,
+    'border-link': hasBordered,
+    'active-child': !disabled && hasActiveChild,
+    active: !disabled && isActive,
+    disabled: disabled
+  });
+
+  const handleClick: React.MouseEventHandler = (event) => {
+    if (!href) {
+      event.preventDefault();
+      !disabled && props.onClick && props.onClick(id);
+    }
+  };
+
+  return (
+    <li className="nav-item">
+      <a
+        className={linkClassName}
+        href={href || '#'}
+        onClick={handleClick}
+        aria-disabled={disabled}
+        tabIndex={disabled ? -1 : undefined}>
+        {hasIcon ? (
+          <div className="nav-icon">
+            <i className="bx bxs-user-circle"></i>
+            <span>{name}</span>
+          </div>
+        ) : (
+          <span>{name}</span>
+        )}
+      </a>
+      {!disabled && (isActive || hasActiveChild) && children && (
+        <ul className={listClasses}>
+          {children.map((item) => (
+            <NavItemComponent
+              {...item}
+              key={item.id}
+              level={level + 1}
+              onClick={props.onClick}
+              selected={selected}
+              hasIcon={hasIcon}
+            />
           ))}
         </ul>
       )}
