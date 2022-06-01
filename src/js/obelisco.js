99881b4a36a6d07899a5ce512dc8c21933f04f85
Alan199912 <alan199912@gmail.com>
2022-06-01 16:47:09 -0300

    fix: Se agregaron comentarios y se elimino una funcion que no tenia uso

--- src/js/obelisco.js
+++ src/js/obelisco.js
@@ -1,45 +1,32 @@
-function dropFunctionability(e) {
-  const CLASS_DROP_DOWN_LIST = e.path[1].classList;
-  const drop = document.querySelector(`.${CLASS_DROP_DOWN_LIST[0]}`);
-
-  if (!drop) return;
-
-  if (
-    e.target.classList.contains('dropdown-item') ||
-    e.target.type === 'checkbox' ||
-    e.target.classList.contains('dropdown-list') ||
-    e.target.classList.contains('bx-x') ||
-    e.target.id === 'item'
-  ) {
-    return;
-  }
-
-  if (drop.classList.contains('visible')) {
-    drop.classList.remove('visible');
-  } else {
-    drop.classList.add('visible');
-  }
-
-  document.addEventListener('mousedown', function (e) {
-    if (e.target.classList.contains('cancel-input')) {
-      return;
-    }
-
-    if (drop.classList.contains('visible') && !drop.contains(e.target)) {
-      drop.classList.remove('visible');
-    }
-  });
-}
-
 const CLASS_VISIBLE = 'visible';
 const CLASS_CHECK_LIST = '.dropdown-check-list';
+const CLASS_SHOW = 'show';
+const ARR_CLASS_DROPDOWN_CHECK_LIST = ['anchor', 'badge-content-item'];
+let dropChildren = null;
+let idDropDownCheckList = null;
+let idDropDownMultiple = null;
+let objDrop = {};
 
+/**
+ * Funcionalidad que verifica si tiene la colleccion de clases.
+ * @param {*} a Se requiere todos los  elemento a verificar mediante el selector.
+ * @param {*} b Se requiere el elemento padre a verificar.
+ * @returns {Boolean} Retorna true si el elemento padre contiene la clase.
+ * Si no tiene la clase retorna false.
+ */
 function collectionHas(a, b) {
   for (var i = 0, len = a.length; i < len; i++) {
     if (a[i] == b) return true;
   }
   return false;
 }
+
+/**
+ * Funcionalidad que encuentra el elemento padre mediante un selector
+ * @param {*} elm Se requiere el elemento al que se le busca el padre.
+ * @param {*} selector Se requiere el selector para identificar el padre.
+ * @returns {Element} Retorna el elemento padre.
+ */
 function findParentBySelector(elm, selector) {
   var all = document.querySelectorAll(selector);
 
@@ -52,8 +39,15 @@ function findParentBySelector(elm, selector) {
   return cur;
 }
 
+/**
+ * Funcionalidad que implica en el dropdown check list.
+ * @param {*} id Se requiere el id para identificar el drop que se quiere mostrar.
+ * Se obtiene el drop, mediante su elemento padre.
+ * Se verifica si tiene la clase `show` y se remueve, de lo contrario se agrega.
+ * Si se ejecuta cualquier otra accion por fuera del dropdown multiple, se remueve la clase `show`
+ */
 function dropDownCheckList(id) {
-  var drop = findParentBySelector(document.getElementById(id), CLASS_CHECK_LIST);
+  const drop = findParentBySelector(document.getElementById(id), CLASS_CHECK_LIST);
 
   if (!drop) return;
 
@@ -70,14 +64,18 @@ function dropDownCheckList(id) {
   });
 }
 
-const CLASS_SHOW = 'show';
-let dropChildren = null;
-
+/**
+ * Funcionalidad que implica en el dropdown multiple
+ * @param {*} id Se requiere el id para identificar el drop que se quiere mostrar.
+ * Se obtiene el drop, su componente hijo y el menu mediante el id.
+ * Se verifica si tiene la clase `show` y se remueve, de lo contrario se agrega.
+ * Si se ejecuta cualquier otra accion por fuera del dropdown multiple, se remueve la clase `show`
+ */
 function dropDownMultiple(id) {
   const drop = document.querySelector(`#drop-${id}`);
-  const dropMenu = document.querySelector(`#drop-menu-${id}`);
-
   dropChildren = drop.children.item(0);
+
+  const dropMenu = document.querySelector(`#drop-menu-${id}`);
 
   if (!dropMenu || !drop) return;
 
@@ -100,10 +98,11 @@ function dropDownMultiple(id) {
   });
 }
 
-const ARR_CLASS_DROPDOWN_CHECK_LIST = ['anchor', 'badge-content-item'];
-let objDrop = {};
-let idDropDownCheckList = null;
-
+/**
+ * Se verifica si tiene las clases `anchor` o `badge-content-item` desde donde se esta ejecutando la funcionalidad
+ * @param e {Event} Se requiere el evento para obtener los valores de las clases.
+ * Si no tienen valor directamente no se ejecuta la funcionalidad.
+ */
 const hasCheckListClass = (event) => {
   if (!event?.target?.['classList'].value || event?.target?.['classList'].value === '') return;
   return ARR_CLASS_DROPDOWN_CHECK_LIST.some(
@@ -114,6 +113,14 @@ const hasCheckListClass = (event) => {
   );
 };
 
+/**
+ * Funcionalidad que implica en el click sobre el icono `x` en los elementos del dropdown check list
+ * @param e {Event} Se requiere el evento para prevenir el comportamiento por defecto del navegador
+ * y obtener el nombre del texto del nodo padre, es decir, el valor dentro del <span>
+ * Se asocia el input con el valor del texto del nodo padre,
+ * se le agrega el valor false al check, se obtiene el id del input asociado y se elimina el elemento tanto dentro array como del arbol DOM.
+ * Si el array esta vacio, se agrega un elemento dentro del arbol DOM con el texto `Todas`
+ */
 const removeItem = (e) => {
   e.preventDefault();
   const text = e.target.parentNode.textContent.trim();
@@ -121,20 +128,26 @@ const removeItem = (e) => {
   const inputAssociated = document.querySelector(`input[type="checkbox"][value="${text}"]`);
 
   inputAssociated.checked = false;
-  const getItByInput = inputAssociated.dataset.filterItem.split('filter-')[1];
+  const getIdByInput = inputAssociated.dataset.filterItem.split('filter-')[1];
 
-  objDrop[getItByInput] = objDrop[getItByInput].filter((el) => el !== text);
+  objDrop[getIdByInput] = objDrop[getIdByInput].filter((el) => el !== text);
 
   e.target.parentNode.parentNode.remove();
 
-  if (objDrop[getItByInput].length === 0) {
+  if (objDrop[getIdByInput].length === 0) {
     var initSpan = document.createElement('span');
     initSpan.innerHTML = 'Todas';
-    const anchor = document.querySelector(`#${getItByInput}`);
+    const anchor = document.querySelector(`#${getIdByInput}`);
     anchor.appendChild(initSpan);
   }
 };
 
+/**
+ * Funcionalidad que implica en el menu sobre los items del dropdown check list
+ * @param event {Event} Se requiere el evento para verificar si es una etiqueta input,si esto ocurre,
+ * se crea una equiteca <span> dentro de la clase `anchor`.
+ * se verifica si no esta checked, se ejecuta la funcionalidad de agregar o quitar un item del dropdown check list
+ */
 function dropDownCheckListItems(event) {
   if (!objDrop[idDropDownCheckList] || objDrop[idDropDownCheckList].length === 0) {
     objDrop[idDropDownCheckList] = [];
@@ -174,7 +187,11 @@ function dropDownCheckListItems(event) {
   }
 }
 
-let idDropDownMultiple = null;
+/**
+ * Funcionalidad que implica en el menu sobre los items del dropdown multiple
+ * @param event {Event} Se requiere el evento para verificar si es una etiqueta input.
+ * Si es asi se verifica si no esta checked y se ejecuta la funcionalidad de agregar o quitar un item del dropdown multiple
+ */
 function dropDownSelectedItems(event) {
   if (!objDrop[idDropDownMultiple] || objDrop[idDropDownMultiple].length === 0) {
     objDrop[idDropDownMultiple] = [];
@@ -195,11 +212,13 @@ function dropDownSelectedItems(event) {
   }
 }
 
+// Init
 (function (window, document) {
   document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener(
       'click',
       function (event) {
+        // Dropdown check list
         if (hasCheckListClass(event)) {
           if (event.target.localName === 'i') return;
 
@@ -215,6 +234,7 @@ function dropDownSelectedItems(event) {
           dropDownCheckListItems(event);
         }
 
+        // Dropdown multiple
         if (!!event?.target.dataset.oToggle && event?.target.dataset.oToggle.includes('dropdown-')) {
           idDropDownMultiple = event.target.dataset.oToggle.split('dropdown-')[1];
 
