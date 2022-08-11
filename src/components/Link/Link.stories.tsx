2f104ae92c36efdd9678cda635804734630ef0b5
Jesus Avendaño <Jesus.fy.1997@gmail.com>
2022-08-11 11:25:25 -0300

    fix: Se agrega la clase download-link al componente enlace

--- src/components/Link/Link.stories.tsx
+++ src/components/Link/Link.stories.tsx
@@ -52,6 +52,15 @@ export const Descarga = (): JSX.Element => {
         as he was, for all he was so gifted. And when it come to fair and square jumping on a dead level, he could get
         over more ground at one straddle than any animal of his breed you ever see.
       </p>
+
+      <p>
+        You never see a frog so modest and{' '}
+        <a className="btn btn-secondary download-link" href="#" target="_blank" rel="noreferrer">
+          enlace de descarga
+        </a>{' '}
+        as he was, for all he was so gifted. And when it come to fair and square jumping on a dead level, he could get
+        over more ground at one straddle than any animal of his breed you ever see.
+      </p>
     </div>
   );
 };
