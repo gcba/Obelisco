2218a4c5fc3df81d44a17eda539141bad6a19375
lautarorodriguez96 <70086268+lautarorodriguez96@users.noreply.github.com>
2022-08-14 15:26:52 -0300

    feat: Link list (#90)
    * feat: Nuevo componente panel destacado grande
    
    * merge entre componentes panel
    
    * lista de enlaces
    
    * cambio tamaño de titulo e icono descarga
    
    * agrego extension h3 y formateo de scss
    
    Co-authored-by: Alan199912 <alan199912@gmail.com>

--- src/components/Panel/PanelSmall.stories.tsx
+++ src/components/Panel/PanelSmall.stories.tsx
@@ -17,7 +17,7 @@ export const Completo = (): JSX.Element => {
     <div className="panel-container">
       <div className="card card-simple panel-sm">
         <img src="cards/paseobajo-wide.png" className="rounded-lg mb-4" width={154} height={154}  alt="descripción alternativa" />
-        <div className="card-body">          
+        <div className="card-body">
           <h3 className="card-title">Título de tarjeta</h3>
           <p className="card-text">
             Recomendamos en este espacio no utilizar más de 3 líneas, para generar
