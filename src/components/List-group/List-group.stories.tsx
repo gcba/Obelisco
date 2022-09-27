e408a9ad824b830232e341670e6a8378b844d38a
Lautaro Rodriguez <lar1996.vs@gmail.com>
2022-09-27 17:03:31 -0300

    fix merge

--- src/components/List-group/List-group.stories.tsx
+++ src/components/List-group/List-group.stories.tsx
@@ -4,17 +4,11 @@ import React from 'react';
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
 
-<<<<<<< HEAD
-=======
-import './List-group.stories.scss';
->>>>>>> e24971ec4cb64596dba64ac20d83f72f57cd6ce9
 import { AccesIcon } from './AccesIcon.stories';
 import { AccessList } from './AccessList.stories';
 import { AccessListCategory } from './AccessListCategory.stories';
-import { BigAccess } from './BigAccess.stories';
 import './List-group.stories.scss';
 import { AccessListDescriptive } from './ListAccessDescriptive.stories';
-import { SmallAccess } from './SmallAcces.stories';
 import { SmallAccessDescription } from './SmallAccessDescription.stories';
 
 // Configuración del general del componente
