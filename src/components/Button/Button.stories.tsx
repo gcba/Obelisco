322b1563179523a49ab5170891f42b523671a065
Victoria Raquel Nastasi <vnastasi@buenosaires.gob.ar>
2023-01-05 11:08:26 -0300

    feat: se agrega Boton con iconos y Boton en caja, y se modifican estilos de botones

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -114,9 +114,159 @@ export const ConBordes = (): JSX.Element => {
           {type}
         </Button>
       ))}
-      <button type="button" className="btn btn-outline-primary" disabled>
-        Deshabilitado
-      </button>
+      <a className="btn btn-outline-primary" href="#" target="_blank">
+        enlace
+      </a>
     </div>
+  );
+};
+
+export const ConIconos = (): JSX.Element => {
+  return (
+    <>
+      <div className="btn-wrapper">
+        <button type="button" className="btn btn-primary btn-sm">
+          <i className="bx bxs-car btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-primary">
+          <i className="bx bxs-car btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-primary btn-lg">
+          <i className="bx bxs-car btn-icon"></i>
+          Botón
+        </button>
+      </div>
+      <div className="btn-wrapper">
+        <button type="button" className="btn btn-success btn-sm">
+          <i className="bx bxs-check-shield btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-success">
+          <i className="bx bxs-check-shield btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-success btn-lg">
+          <i className="bx bxs-check-shield btn-icon"></i>
+          Botón
+        </button>
+      </div>
+      <div className="btn-wrapper">
+        <button type="button" className="btn btn-danger btn-sm">
+          <i className="bx bx-log-out btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-danger">
+          <i className="bx bx-log-out btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-danger btn-lg">
+          <i className="bx bx-log-out btn-icon"></i>
+          Botón
+        </button>
+      </div>
+      <div className="btn-wrapper">
+        <button type="button" className="btn btn-secondary btn-sm">
+          <i className="bx bx-message-dots btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-secondary">
+          <i className="bx bx-message-dots btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-secondary btn-lg">
+          <i className="bx bx-message-dots btn-icon"></i>
+          Botón
+        </button>
+      </div>
+      <div className="btn-wrapper">
+        <button type="button" className="btn btn-link btn-sm">
+          <i className="bx bxs-shield-x btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-link">
+          <i className="bx bxs-shield-x btn-icon"></i>
+          Botón
+        </button>
+        <button type="button" className="btn btn-link btn-lg">
+          <i className="bx bxs-shield-x btn-icon"></i>
+          Botón
+        </button>
+      </div>
+    </>
+  );
+};
+
+export const EnCaja = (): JSX.Element => {
+  return (
+    <>
+      <div className="btn-wrapper-container">
+        <div className="btn-container">
+          <div className="card">
+            <button type="button" className="btn btn-primary btn-block btn-sm">
+              Botón
+            </button>
+          </div>
+        </div>
+        <div className="btn-container">
+          <div className="card">
+            <button type="button" className="btn btn-primary btn-block">
+              Botón
+            </button>
+          </div>
+        </div>
+        <div className="btn-container">
+          <div className="card">
+            <button type="button" className="btn btn-primary btn-block btn-lg">
+              Botón
+            </button>
+          </div>
+        </div>
+        <div className="btn-container">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
+              <p className="card-text">Si tenés un trámite iniciado podés continuarlo desde acá.</p>
+            </div>
+            <button type="button" className="btn btn-primary btn-block">
+              Iniciar trámite
+            </button>
+          </div>
+        </div>
+        <div className="btn-container">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
+            </div>
+            <button type="button" className="btn btn-primary btn-block">
+              Iniciar trámite
+            </button>
+          </div>
+        </div>
+        <div className="btn-container">
+          <div className="card bg-light">
+            <div className="card-body">
+              <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
+              <p className="card-text">Si tenés un trámite iniciado podés continuarlo desde acá.</p>
+            </div>
+            <button type="button" className="btn btn-primary btn-block">
+              Iniciar trámite
+            </button>
+          </div>
+        </div>
+        <div className="btn-container">
+          <div className="card card-simple">
+            <div className="card-body">
+              <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
+              <p className="card-text">Si tenés un trámite iniciado podés continuarlo desde acá.</p>
+            </div>
+            <button type="button" className="btn btn-primary btn-block">
+              Iniciar trámite
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
