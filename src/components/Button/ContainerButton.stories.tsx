b8e323b5c69132d9a700066d8b59b082fa1a5eda
Victoria Raquel Nastasi <vnastasi@buenosaires.gob.ar>
2023-01-12 12:01:28 -0300

    fix: modificaciones en botones en caja, button.scss

--- src/components/Button/ContainerButton.stories.tsx
+++ src/components/Button/ContainerButton.stories.tsx
@@ -13,58 +13,58 @@ export default {
 
 export const SoloBoton = (): JSX.Element => {
   return (
-    <>
-      <div className="btn-container">
-        <div className="card">
+    <div className="btn-container">
+      <div className="card">
+        <div className="card-body">
           <button type="button" className="btn btn-primary btn-block btn-sm">
             Botón
           </button>
         </div>
       </div>
-      <div className="btn-container">
-        <div className="card">
+      <div className="card">
+        <div className="card-body">
           <button type="button" className="btn btn-primary btn-block">
             Botón
           </button>
         </div>
       </div>
-      <div className="btn-container">
-        <div className="card">
+      <div className="card">
+        <div className="card-body">
           <button type="button" className="btn btn-primary btn-block btn-lg">
             Botón
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 SoloBoton.story = { name: 'Solo Botón' };
 
 export const ConEnlace = (): JSX.Element => {
   return (
-    <>
-      <div className="btn-container">
-        <div className="card">
+    <div className="btn-container">
+      <div className="card">
+        <div className="card-body">
           <a className="btn btn-primary btn-block btn-sm" href="#" target="_blank">
             Enlace
           </a>
         </div>
       </div>
-      <div className="btn-container">
-        <div className="card">
+      <div className="card">
+        <div className="card-body">
           <a className="btn btn-primary btn-block" href="#" target="_blank">
             Enlace
           </a>
         </div>
       </div>
-      <div className="btn-container">
-        <div className="card">
+      <div className="card">
+        <div className="card-body">
           <a className="btn btn-primary btn-block btn-lg" href="#" target="_blank">
             Enlace
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
@@ -75,10 +75,10 @@ export const Completo = (): JSX.Element => {
         <div className="card-body">
           <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
           <p className="card-text">Si tenés un trámite iniciado podés continuarlo desde acá.</p>
+          <a className="btn btn-primary btn-block" href="#" target="_blank">
+            Iniciar trámite
+          </a>
         </div>
-        <a className="btn btn-primary btn-block" href="#" target="_blank">
-          Iniciar trámite
-        </a>
       </div>
     </div>
   );
@@ -89,11 +89,11 @@ export const SoloTitulo = (): JSX.Element => {
     <div className="btn-container">
       <div className="card">
         <div className="card-body">
-          <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
+          <h4 className="card-title mb-3">Iniciá el trámite y completá todos los pasos</h4>
+          <a className="btn btn-primary btn-block" href="#" target="_blank">
+            Iniciar trámite
+          </a>
         </div>
-        <a className="btn btn-primary btn-block" href="#" target="_blank">
-          Iniciar trámite
-        </a>
       </div>
     </div>
   );
@@ -107,10 +107,10 @@ export const ConFondo = (): JSX.Element => {
         <div className="card-body">
           <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
           <p className="card-text">Si tenés un trámite iniciado podés continuarlo desde acá.</p>
+          <a className="btn btn-primary btn-block" href="#" target="_blank">
+            Iniciar trámite
+          </a>
         </div>
-        <a className="btn btn-primary btn-block" href="#" target="_blank">
-          Iniciar trámite
-        </a>
       </div>
     </div>
   );
@@ -123,10 +123,10 @@ export const SinBorde = (): JSX.Element => {
         <div className="card-body">
           <h4 className="card-title">Iniciá el trámite y completá todos los pasos</h4>
           <p className="card-text">Si tenés un trámite iniciado podés continuarlo desde acá.</p>
+          <a className="btn btn-primary btn-block" href="#" target="_blank">
+            Iniciar trámite
+          </a>
         </div>
-        <a className="btn btn-primary btn-block" href="#" target="_blank">
-          Iniciar trámite
-        </a>
       </div>
     </div>
   );
