147f283e39e4012aae453ceda7f086e7469d9bb0
Victoria Nastasi <victorianastasi@gmail.com>
2023-01-17 12:35:33 -0300

    feat: se agrega el componente Botones con bordes (#111)
    * feat: se agrega el componente Botones con bordes
    
    * fix: se modifica scss de button
    
    * feat: se agrega Boton con iconos y Boton en caja, y se modifican estilos de botones
    
    * fix: se agrega enlace en Boton en caja
    
    * fix: modificaciones en boton en caja, boton con icono, icono de enlace de descarga y espaciado de boton en storybook
    
    * fix: agrego acento en Solo Título y Solo Botón
    
    * fix: modificacion de botones con iconos, color de enlaces, focus de botones, y clase btn-wrapper
    
    * fix: modificaciones en botones en caja, button.scss
    
    * fix: se eliminan variantes de botones en caja
    
    * fix: modificación en espaciado de icono y en boton con icono: se agrega boton con borde y se deja solo boton primary
    
    Co-authored-by: Victoria Raquel Nastasi <vnastasi@buenosaires.gob.ar>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,18 +7,23 @@ export interface ButtonProps {
   block?: boolean;
   disabled?: boolean;
   className?: string;
+  outline?: boolean;
+  icon?: string;
 }
 
 export const Button: React.FC<ButtonProps> = (props: React.PropsWithChildren<ButtonProps>) => {
-  const { type, size, block, disabled, children } = props;
+  const { type, size, block, disabled, children, outline, icon } = props;
 
   let className = `btn btn-${type}`;
+  if (outline) className = `btn btn-outline-${type}`;
   if (!!size && size !== 'default') className += ` btn-${sizeToClass(size)}`;
   if (block) className += ` btn-block`;
   if (props.className) className += ` ${props.className}`;
+  if (icon) className += ` btn-icon`;
 
   return (
     <button type="button" className={className} disabled={disabled}>
+      {icon && <i className={`bx ${icon}`}></i>}
       {children}
     </button>
   );
