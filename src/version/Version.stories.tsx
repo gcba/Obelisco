0fa0f50068494eb09b997599440d7a9a8bd34c9b
Victoria Raquel Nastasi <vnastasi@buenosaires.gob.ar>
2023-02-07 16:45:44 -0300

    fix: se modifica version

--- src/version/Version.stories.tsx
+++ src/version/Version.stories.tsx
@@ -10,13 +10,13 @@ export default {
 
 export const Info = (): JSX.Element => (
   <>
-    <h1>Versión Obelisco 1.46.0</h1>
+    <h1>Versión Obelisco 1.46.1</h1>
     <p>
       Acceso al release:{' '}
       <a
         className="access"
         target="_blank"
-        href="https://github.com/gcba/Obelisco/releases/tag/v1.46.0"
+        href="https://github.com/gcba/Obelisco/releases/tag/v1.46.1"
         rel="noreferrer">
         Github
       </a>
@@ -25,5 +25,5 @@ export const Info = (): JSX.Element => (
 );
 
 Info.story = {
-  name: '1.46.0'
+  name: '1.46.1'
 };
