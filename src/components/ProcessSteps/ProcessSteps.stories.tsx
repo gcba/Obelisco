acb6eded3c3c11a4bd1718d71a93a6f2af669122
matilope <mati02171996@gmail.com>
2023-02-16 17:15:58 -0300

    fix: se saco la variacion de colores

--- src/components/ProcessSteps/ProcessSteps.stories.tsx
+++ src/components/ProcessSteps/ProcessSteps.stories.tsx
@@ -5,9 +5,6 @@ import './ProcessSteps.stories.scss';
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
 
-// Components
-import { List } from '.';
-
 // Configuración general del componente
 export default {
   title: 'Componentes|Pasos de trámite',
@@ -15,62 +12,20 @@ export default {
 };
 
 export const Pasos = (): JSX.Element => (
-  <>
-    <div className="lists-wrapper">
-      <List
-        items={[
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          },
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          },
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          }
-        ]}
-      />
-    </div>
-    <div className="lists-wrapper">
-      <List
-        items={[
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          },
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          },
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          }
-        ]}
-        className="list-steps-success"
-      />
-    </div>
-    <div className="lists-wrapper">
-      <List
-        items={[
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          },
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          },
-          {
-            title: 'Esta es la información destacada de la lista',
-            description: 'Esta es la descripción informativa de una lista '
-          }
-        ]}
-        className="list-steps-information"
-      />
-    </div>
-  </>
+  <div className="lists-wrapper">
+    <ul className="list-steps">
+      <li className="list-steps-item">
+        <h5>Esta es la información destacada de la lista</h5>
+        <p>Esta es la descripción informativa de una lista</p>
+      </li>
+      <li className="list-steps-item">
+        <h5>Esta es la información destacada de la lista</h5>
+        <p>Esta es la descripción informativa de una lista</p>
+      </li>
+      <li className="list-steps-item">
+        <h5>Esta es la información destacada de la lista</h5>
+        <p>Esta es la descripción informativa de una lista</p>
+      </li>
+    </ul>
+  </div>
 );
