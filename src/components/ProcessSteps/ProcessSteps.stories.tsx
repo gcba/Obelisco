1c9bc578e0e8813a9ff5400fbde26c3e4a433748
matilope <mati02171996@gmail.com>
2023-02-17 12:01:22 -0300

    fix: se arreglaron propiedades en el css para que quede identico al figma

--- src/components/ProcessSteps/ProcessSteps.stories.tsx
+++ src/components/ProcessSteps/ProcessSteps.stories.tsx
@@ -12,20 +12,18 @@ export default {
 };
 
 export const Pasos = (): JSX.Element => (
-  <div className="lists-wrapper">
-    <ul className="list-steps">
-      <li className="list-steps-item">
-        <h5>Esta es la información destacada de la lista</h5>
-        <p>Esta es la descripción informativa de una lista</p>
-      </li>
-      <li className="list-steps-item">
-        <h5>Esta es la información destacada de la lista</h5>
-        <p>Esta es la descripción informativa de una lista</p>
-      </li>
-      <li className="list-steps-item">
-        <h5>Esta es la información destacada de la lista</h5>
-        <p>Esta es la descripción informativa de una lista</p>
-      </li>
-    </ul>
-  </div>
+  <ul className="list-steps">
+    <li className="list-steps-item">
+      <h5>Esta es la información destacada de la lista</h5>
+      <p>Esta es la descripción informativa de una lista</p>
+    </li>
+    <li className="list-steps-item">
+      <h5>Esta es la información destacada de la lista</h5>
+      <p>Esta es la descripción informativa de una lista</p>
+    </li>
+    <li className="list-steps-item">
+      <h5>Esta es la información destacada de la lista</h5>
+      <p>Esta es la descripción informativa de una lista</p>
+    </li>
+  </ul>
 );
