a9646ddff1aacbe9180e92147713791c675d0163
Jesus Avendaño <Jesus.fy.1997@gmail.com>
2023-03-02 15:19:10 -0300

    fix: mejoras al scss de tooltip

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -14,18 +14,6 @@ export default {
 export const Tooltip = (): JSX.Element => {
   return (
     <div className="tooltip-container">
-      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
-        <span className="material-icons-round">info</span>
-      </div>
-      <br />
-      <br />
-      <br />
-      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
-        <i className="bx bxs-car"></i>
-      </div>
-      <br />
-      <br />
-      <br />
       <button
         type="button"
         className="btn btn-link"
@@ -99,6 +87,26 @@ export const Tooltip = (): JSX.Element => {
       <button
         type="button"
         className="btn btn-link"
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="left-top">
+        LEFT-TOP
+      </button>
+      <br />
+      <br />
+      <br />
+      <button
+        type="button"
+        className="btn btn-link"
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="left-bottom">
+        LEFT-BOTTOM
+      </button>
+      <br />
+      <br />
+      <br />
+      <button
+        type="button"
+        className="btn btn-link"
         data-tooltip="Esta es la descripción de un tooltip"
         data-flow="right">
         RIGHT
@@ -106,16 +114,257 @@ export const Tooltip = (): JSX.Element => {
       <br />
       <br />
       <br />
+      <button
+        type="button"
+        className="btn btn-link"
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="right-top">
+        RIGHT-TOP
+      </button>
       <br />
       <br />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur{' '}
-        <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
-          LINK
-        </a>{' '}
-        elit. Debitis molestiae suscipit laudantium nisi corruptipraesentium perspiciatis obcaecati consectetur, est
-        illum dolorum aperiam sint deleniti molestias fugit esse excepturi. Laboriosam, sint.
-      </p>
+      <br />
+      <button
+        type="button"
+        className="btn btn-link"
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="right-bottom">
+        RIGHT-BOTTOM
+      </button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        TOP
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="top-left">
+        TOP-LEFT
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="top-right">
+        TOP-RIGHT
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="bottom">
+        BOTTOM
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="bottom-left">
+        BOTTOM-LEFT
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="bottom-right">
+        BOTTOM-RIGHT
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="left">
+        LEFT
+      </a>
+      <br />
+      <br />
+      <br />
+      <a
+        href=""
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="left-top">
+        LEFT-TOP
+      </a>
+      <br />
+      <br />
+      <br />
+      <a
+        href=""
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="left-bottom">
+        LEFT-BOTTOM
+      </a>
+      <br />
+      <br />
+      <br />
+      <a href="" data-tooltip="Esta es la descripción de un tooltip" data-flow="right">
+        RIGHT
+      </a>
+      <br />
+      <br />
+      <br />
+      <a
+        href=""
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="right-top">
+        RIGHT-TOP
+      </a>
+      <br />
+      <br />
+      <br />
+      <a
+        href=""
+        data-tooltip="Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip,Esta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltipEsta es la descripción de un tooltip, esta es la descripción de un tooltip, esta es la descripción de un tooltip"
+        data-flow="right-bottom">
+        RIGHT-BOTTOM
+      </a>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <span className="material-icons-round">info</span>
+      </div>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div data-tooltip="Esta es la descripción de un tooltip" data-flow="top">
+        <i className="bx bxs-info-circle"></i>
+      </div>
       <br />
       <br />
       <br />
