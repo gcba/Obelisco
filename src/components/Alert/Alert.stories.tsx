454f8d45d2943323477884434de48de8a0e1235d
Alan199912 <alan199912@gmail.com>
2023-03-03 10:44:53 -0300

    feat: alert dismissible

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -14,13 +14,27 @@ export default {
   decorators: [withA11y]
 };
 
-export const Texto = (): JSX.Element => {
+export const SimpleConCierre = (): JSX.Element => {
+  return (
+    <div className="alert-wrapper">
+      <div className="alert alert-danger alert-dismissible show fade" role="alert">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, fugiat!&nbsp;
+          <a href="#">con enlaces.</a>
+        </p>
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close"></button>
+      </div>
+    </div>
+  );
+};
+
+export const SimpleConIcono = (): JSX.Element => {
   return (
     <div className="alert-wrapper">
       <div className="alert alert-danger" role="alert">
         <p>
-          Un texto simple para una alerta simple, incluso&nbsp;
-          <a href="#">con hipervínculos</a>.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, fugiat!&nbsp;
+          <a href="#">con enlaces.</a>
         </p>
       </div>
     </div>
