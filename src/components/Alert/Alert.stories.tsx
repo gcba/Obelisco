9fce19e7e44d8bee4fa74e86644d994689759efd
Alan199912 <alan199912@gmail.com>
2023-03-20 10:55:57 -0300

    fix: ordenamiento y renombre de las stories

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -14,52 +14,7 @@ export default {
   decorators: [withA11y]
 };
 
-export const SimpleConCierre = (): JSX.Element => {
-  return (
-    <div className="alert-wrapper">
-      <div className="alert alert-danger alert-dismissible show fade" role="alert">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, fugiat!&nbsp;
-          <a href="#">con enlaces.</a>
-        </p>
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close"></button>
-      </div>
-    </div>
-  );
-};
-
-export const SimpleConIcono = (): JSX.Element => {
-  return (
-    <div className="alert-wrapper">
-      <div className="alert alert-danger" role="alert">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, fugiat!&nbsp;
-          <a href="#">con enlaces.</a>
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export const Titulo = (): JSX.Element => {
-  return (
-    <div className="alert-wrapper">
-      <div className="alert alert-danger" role="alert">
-        <p>
-          <strong>Este es un destacado de una alerta (opcional). </strong>
-          Un texto simple para una alerta simple, incluso&nbsp;
-          <a href="#">con hipervínculos</a>.
-        </p>
-      </div>
-    </div>
-  );
-};
-
-Titulo.story = {
-  name: 'Destacado y texto'
-};
-
-export const Colores = (): JSX.Element => {
+export const Simple = (): JSX.Element => {
   return (
     <div className="alert-wrapper">
       {alertTypes.map((type) => (
@@ -70,3 +25,114 @@ export const Colores = (): JSX.Element => {
     </div>
   );
 };
+
+export const SimpleClose = (): JSX.Element => {
+  return (
+    <div className="alert-wrapper">
+      {alertTypes.map((type) => (
+        <div key={type} className={`alert alert-${type} alert-dismissible show fade`} role="alert">
+          <p>Un texto simple para una alerta simple.</p>
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span className="material-icons-round">close</span>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+SimpleClose.story = {
+  name: 'Con cierre'
+};
+
+export const SimpleHighlight = (): JSX.Element => {
+  return (
+    <div className="alert-wrapper">
+      {alertTypes.map((type) => (
+        <div key={type} className={`alert alert-${type}`} role="alert">
+          <p>
+            <strong>Este es un destacado de una alerta (opcional). </strong>
+            Un texto simple para una alerta simple.
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+SimpleHighlight.story = {
+  name: 'Con destacado'
+};
+
+export const SimpleHiperlink = (): JSX.Element => {
+  return (
+    <div className="alert-wrapper">
+      {alertTypes.map((type) => (
+        <div key={type} className={`alert alert-${type}`} role="alert">
+          <p>
+            Un texto simple para una alerta simple, incluso&nbsp;
+            <a href="#">con enlaces.</a>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+SimpleHiperlink.story = {
+  name: 'Con hipervínculo'
+};
+
+export const ListLinks = (): JSX.Element => {
+  return (
+    <div className="alert-wrapper">
+      <div className="alert alert-danger fade show" role="alert">
+        <p>
+          <strong>Este es un destacado de una alerta (opcional). </strong>
+        </p>
+        <ol className="list-links">
+          <li>
+            <a href="#">Un texto simple para un enlace en una lista de alerta</a>
+          </li>
+
+          <li>
+            <a href="#">Un texto simple para un enlace en una lista de alerta</a>
+          </li>
+
+          <li>
+            <a href="#">Un texto simple para un enlace en una lista de alerta</a>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+ListLinks.story = { name: 'Con lista descriptiva' };
+
+export const List = (): JSX.Element => {
+  return (
+    <div className="alert-wrapper">
+      <div className="alert alert-danger fade show" role="alert">
+        <p>
+          <strong>Este es un destacado de una alerta (opcional). </strong>
+        </p>
+        <ol>
+          <li>
+            <span>Un texto simple para un enlace en una lista de alerta</span>
+          </li>
+
+          <li>
+            <span>Un texto simple para un enlace en una lista de alerta</span>
+          </li>
+
+          <li>
+            <span>Un texto simple para un enlace en una lista de alerta</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+List.story = { name: 'Con lista de enlaces' };
