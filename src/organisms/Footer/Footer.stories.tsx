0b011cd87c13abac379c1482ca3d0af2439456f5
Lautaro Rodriguez <lar1996.vs@gmail.com>
2023-04-24 15:21:26 -0300

    cambio de link font y espaciados redes

--- src/organisms/Footer/Footer.stories.tsx
+++ src/organisms/Footer/Footer.stories.tsx
@@ -73,7 +73,7 @@ const LegalSection = (): JSX.Element => (
             <a href="https://boletinoficial.buenosaires.gob.ar">Boletín oficial</a>
           </li>
           <li className="list-inline-item">
-            <a href="https://www.buenosaires.gob.ar/innovacion/ciudadinteligente/terminos-y-condiciones">
+            <a href="https://buenosaires.gob.ar/jefaturadegabinete/innovacion/terminos-y-condiciones">
               Términos y condiciones
             </a>
           </li>
