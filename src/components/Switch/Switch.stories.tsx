48f57500e831569c92ba0a06e02360fdc7abbf5a
Jesus Avendaño <Jesus.fy.1997@gmail.com>
2023-06-02 16:19:01 -0300

    fix: mejoras en el componente

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -1,5 +1,6 @@
 // Base
 import React from 'react';
+import './Switch.stories.scss';
 
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
@@ -14,46 +15,79 @@ export const Switch = (): JSX.Element => {
   return (
     <>
       <label className="switch">
-        <input type="checkbox" id="switch"></input>
+        <input type="checkbox" id="switch-1"></input>
         <span className="slider"></span>
       </label>
-      <label htmlFor="switch" className="switch-label">
+      <label htmlFor="switch-1" className="switch-label">
+        Label
+      </label>
+      <label className="switch">
+        <input type="checkbox" id="switch-2" checked></input>
+        <span className="slider"></span>
+      </label>
+      <label htmlFor="switch-2" className="switch-label">
         Label
       </label>
       <br />
       <br />
       <label className="switch switch-dark">
-        <input type="checkbox" id="switch-dark"></input>
+        <input type="checkbox" id="switch-dark-1"></input>
         <span className="slider"></span>
       </label>
-      <label htmlFor="switch-dark" className="switch-label">
+      <label htmlFor="switch-dark-1" className="switch-label">
+        Label
+      </label>
+      <label className="switch switch-dark">
+        <input type="checkbox" id="switch-dark-2" checked></input>
+        <span className="slider"></span>
+      </label>
+      <label htmlFor="switch-dark-2" className="switch-label">
         Label
       </label>
       <br />
       <br />
       <label className="switch">
-        <input type="checkbox" id="switch-disable" disabled></input>
+        <input type="checkbox" id="switch-disable-1" disabled></input>
         <span className="slider"></span>
       </label>
-      <label htmlFor="switch-disable" className="switch-label">
+      <label htmlFor="switch-disable-1" className="switch-label disabled">
+        Label
+      </label>
+      <label className="switch">
+        <input type="checkbox" id="switch-disable-2" disabled checked></input>
+        <span className="slider"></span>
+      </label>
+      <label htmlFor="switch-disable-2" className="switch-label disabled">
         Label
       </label>
       <br />
       <br />
       <label className="switch switch-sm">
-        <input type="checkbox" id="switch-sm"></input>
+        <input type="checkbox"></input>
+        <span className="slider"></span>
+      </label>
+      <label className="switch switch-sm">
+        <input type="checkbox" checked></input>
         <span className="slider"></span>
       </label>
       <br />
       <br />
       <label className="switch switch-dark switch-sm">
-        <input type="checkbox" id="switch-dark-sm"></input>
+        <input type="checkbox"></input>
+        <span className="slider"></span>
+      </label>
+      <label className="switch switch-dark switch-sm">
+        <input type="checkbox" checked></input>
         <span className="slider"></span>
       </label>
       <br />
       <br />
       <label className="switch switch-sm">
-        <input type="checkbox" id="switch-sm" disabled></input>
+        <input type="checkbox" disabled></input>
+        <span className="slider"></span>
+      </label>
+      <label className="switch switch-sm">
+        <input type="checkbox" disabled checked></input>
         <span className="slider"></span>
       </label>
     </>
