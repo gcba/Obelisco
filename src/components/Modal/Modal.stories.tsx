0fde7e22e841e4c0b2d336caecc80232036e5a0f
Jesus Avendaño <Jesus.fy.1997@gmail.com>
2023-06-26 10:50:06 -0300

    fix: se agregan los cambios nuevos de modal

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -72,3 +72,84 @@ export const Demo = (): JSX.Element => {
 };
 
 Demo.story = { name: 'Demo' };
+
+export const Test = (): JSX.Element => {
+  return (
+    <div>
+      <div className="btn-wrapper">
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal1">
+          Ejemplo 1
+        </button>
+
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal2">
+          Ejemplo 2
+        </button>
+
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal3">
+          Ejemplo 3
+        </button>
+      </div>
+
+      <div className="modal fade" tabIndex={-1} role="dialog" id="exampleModal1">
+        <div className="modal-dialog modal-sm" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">¡Importante!</h4>
+            </div>
+            <hr />
+            <div className="modal-body">
+              <p>
+                Antes de realizar la ficha de salud, tené en cuenta haber realizado los pasos previos correspondientes.
+              </p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-outline-link" data-dismiss="modal">
+                Volver atrás
+              </button>
+              <button type="button" className="btn btn-primary">
+                Continuar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="modal fade" tabIndex={-1} role="dialog" id="exampleModal2">
+        <div className="modal-dialog modal-sm" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Eliminar documento</h4>
+            </div>
+            <hr />
+            <div className="modal-body">
+              <p>El documento que seleccionaste será eliminado.</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">
+                Cancelar
+              </button>
+              <button type="button" className="btn btn-danger">
+                Eliminar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="modal fade" tabIndex={-1} role="dialog" id="exampleModal3">
+        <div className="modal-dialog modal-sm" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Los archivos se cargaron correctamente</h4>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary" data-dismiss="modal">
+                Aceptar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
