9819dddf923d12557046672db02e217745fadf51
Victoria Nastasi <victorianastasi@gmail.com>
2023-07-06 15:50:34 -0300

    fix: se agrega contenedor para correcta visualizacion de header

--- src/organisms/Header/Header.stories.tsx
+++ src/organisms/Header/Header.stories.tsx
@@ -1,5 +1,6 @@
 // Base
 import React from 'react';
+import './Header.stories.scss';
 
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
@@ -12,72 +13,74 @@ export default {
 
 export const HeaderNoLogin = (): JSX.Element => {
   return (
-    <header className="navbar navbar-light navbar-md">
-      <div className="container">
-        <a href="#" className="navbar-brand">
-          <img className="header-logo" src="header/LogoBA.svg" alt="Ciudad de buenos aires" />
-        </a>
-        <div className="navbar-search d-responsive ml-auto">
-          <form className="form-search form-search-sm">
-            <div className="form-group">
-              <input
-                type="search"
-                className="form-control form-control-sm input-search"
-                id="header-search-example-1"
-                name="header-search"
-                placeholder="Buscar..."
-                aria-label="Ingrese su búsqueda"
-              />
-              <button className="reset" type="reset"></button>
-            </div>
-          </form>
-        </div>
-        <button
-          className="navbar-toggler collapsed ml-2"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <a href="#" className="nav-link active">
-                <span>Inicio</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <span>Chateá con BOTI</span>
-              </a>
-            </li>
-            <li className="nav-item d-responsive">
-              <div className="navbar-search">
-                <form className="form-search form-search-sm">
-                  <div className="form-group">
-                    <input
-                      type="search"
-                      className="form-control form-control-sm input-search"
-                      id="header-search-example-2"
-                      name="header-search"
-                      placeholder="Buscar..."
-                      aria-label="Ingrese su búsqueda"
-                    />
-                    <button className="reset" type="reset"></button>
-                  </div>
-                </form>
+    <div className="storybook__container-header">
+      <header className="navbar navbar-light navbar-md">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/LogoBA.svg" alt="Ciudad de buenos aires" />
+          </a>
+          <div className="navbar-search d-responsive ml-auto">
+            <form className="form-search form-search-sm">
+              <div className="form-group">
+                <input
+                  type="search"
+                  className="form-control form-control-sm input-search"
+                  id="header-search-example-1"
+                  name="header-search"
+                  placeholder="Buscar..."
+                  aria-label="Ingrese su búsqueda"
+                />
+                <button className="reset" type="reset"></button>
               </div>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="btn btn-link">
-                <span>Accedé a tu cuenta</span>
-              </a>
-            </li>
-          </ul>
+            </form>
+          </div>
+          <button
+            className="navbar-toggler collapsed ml-2"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="nav nav-pills">
+              <li className="nav-item">
+                <a href="#" className="nav-link active">
+                  <span>Inicio</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <span>Chateá con BOTI</span>
+                </a>
+              </li>
+              <li className="nav-item d-responsive">
+                <div className="navbar-search">
+                  <form className="form-search form-search-sm">
+                    <div className="form-group">
+                      <input
+                        type="search"
+                        className="form-control form-control-sm input-search"
+                        id="header-search-example-2"
+                        name="header-search"
+                        placeholder="Buscar..."
+                        aria-label="Ingrese su búsqueda"
+                      />
+                      <button className="reset" type="reset"></button>
+                    </div>
+                  </form>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="btn btn-link">
+                  <span>Accedé a tu cuenta</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
@@ -87,92 +90,94 @@ HeaderNoLogin.story = {
 
 export const HeaderLogin = (): JSX.Element => {
   return (
-    <header className="navbar navbar-light navbar-lg">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          <img className="header-logo" src="header/LogoBA.svg" alt="Logo de la ciudad de buenos aires" />
-        </a>
-        <div className="navbar-search d-responsive ml-auto">
-          <form className="form-search form-search-sm">
-            <div className="form-group">
-              <input
-                type="search"
-                className="form-control form-control-sm input-search"
-                id="header-search-example-3"
-                name="header-search"
-                aria-label="Ingrese su búsqueda"
-                placeholder="Buscar..."
-              />
-              <button className="reset" type="reset"></button>
-            </div>
-          </form>
-        </div>
-        <button
-          className="navbar-toggler collapsed ml-2"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="list-group-content d-responsive ml-2">
-          <a href="#" aria-label="Cerrar sesión" className="list-group-item list-group-item-logout logout-sm">
-            &nbsp;
+    <div className="storybook__container-header">
+      <header className="navbar navbar-light navbar-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img className="header-logo" src="header/LogoBA.svg" alt="Logo de la ciudad de buenos aires" />
           </a>
-        </div>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <a href="#" className="nav-link active">
-                <span>Inicio</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <span>Chateá con BOTI</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <span>Mi actividad</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <span>Mi cuenta</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" aria-label="Notificaciones" className="nav-link">
-                <i className="bx bxs-bell"></i>
-              </a>
-            </li>
-            <li className="nav-item d-responsive">
-              <div className="navbar-search">
-                <form className="form-search form-search-sm">
-                  <div className="form-group">
-                    <input
-                      type="search"
-                      className="form-control form-control-sm input-search"
-                      id="header-search-example-4"
-                      name="header-search"
-                      aria-label="Ingrese su búsqueda"
-                      placeholder="Buscar..."
-                    />
-                    <button className="reset" type="reset"></button>
-                  </div>
-                </form>
+          <div className="navbar-search d-responsive ml-auto">
+            <form className="form-search form-search-sm">
+              <div className="form-group">
+                <input
+                  type="search"
+                  className="form-control form-control-sm input-search"
+                  id="header-search-example-3"
+                  name="header-search"
+                  aria-label="Ingrese su búsqueda"
+                  placeholder="Buscar..."
+                />
+                <button className="reset" type="reset"></button>
               </div>
-            </li>
-            <li className="nav-item d-responsive">
-              <a href="#" aria-label="Cerrar sesión" className="list-group-item list-group-item-logout logout-sm">
-                &nbsp;
-              </a>
-            </li>
-          </ul>
+            </form>
+          </div>
+          <button
+            className="navbar-toggler collapsed ml-2"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="list-group-content d-responsive ml-2">
+            <a href="#" aria-label="Cerrar sesión" className="list-group-item list-group-item-logout logout-sm">
+              &nbsp;
+            </a>
+          </div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="nav nav-pills">
+              <li className="nav-item">
+                <a href="#" className="nav-link active">
+                  <span>Inicio</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <span>Chateá con BOTI</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <span>Mi actividad</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <span>Mi cuenta</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" aria-label="Notificaciones" className="nav-link">
+                  <i className="bx bxs-bell"></i>
+                </a>
+              </li>
+              <li className="nav-item d-responsive">
+                <div className="navbar-search">
+                  <form className="form-search form-search-sm">
+                    <div className="form-group">
+                      <input
+                        type="search"
+                        className="form-control form-control-sm input-search"
+                        id="header-search-example-4"
+                        name="header-search"
+                        aria-label="Ingrese su búsqueda"
+                        placeholder="Buscar..."
+                      />
+                      <button className="reset" type="reset"></button>
+                    </div>
+                  </form>
+                </div>
+              </li>
+              <li className="nav-item d-responsive">
+                <a href="#" aria-label="Cerrar sesión" className="list-group-item list-group-item-logout logout-sm">
+                  &nbsp;
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
