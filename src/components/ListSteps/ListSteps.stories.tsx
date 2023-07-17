a707182f41cc39737276b602badf14b46f91d81a
matilope <66441937+matilope@users.noreply.github.com>
2023-07-17 14:00:42 -0300

    feat: lista de pasos (#130)
    * fix: cambios en el after de la lista de pasos de tramite
    
    * fix: se saco la variacion de colores
    
    * fix: se arreglaron propiedades en el css para que quede identico al figma
    
    * fix: se centra la barra y se modifica la distancia del circulo y la linea
    
    * fix: se sacaron variables que no se usaban, se agregaron los comentarios en el scss
    
    * fix: modificacion de estilos
    
    * fix: ajuste en preview ts
    
    * fix: se convierte html a react, ajuste en estilos
    
    * fix: modificacion de estilos segun figma
    
    * fix: comentarios pr
    
    * fix: cambios en estilos acorde a figma
    
    * fix: centrado de numeros en before
    
    * fix: ajuste en estilos de numeros en before
    
    * fix: se modifica nombres de archivos y componente segun ficha
    
    * fix: se agrega word-break
    
    * fix: formateo y ajuste de archivo scss
    
    ---------
    
    Co-authored-by: Victoria Nastasi <victorianastasi@gmail.com>

---
+++ src/components/ListSteps/ListSteps.stories.tsx
@@ -0,0 +1,49 @@
+// Base
+import React from 'react';
+import './ListSteps.stories.scss';
+
+// Addons
+import { withA11y } from '@storybook/addon-a11y';
+
+// Components
+import { ListSteps, LISTSTEPS_CONTENT, LISTSTEPS_CONTENT_LARGE } from '.';
+
+// Configuración general del componente
+export default {
+  title: 'Componentes|Listas/Lista de pasos',
+  decorators: [withA11y]
+};
+
+export const ListStepsSizes = (): JSX.Element => (
+  <div className="storybook__wrapper-list-steps">
+    <ListSteps content={LISTSTEPS_CONTENT_LARGE} size="large" />
+    <ListSteps content={LISTSTEPS_CONTENT} />
+    <ListSteps content={LISTSTEPS_CONTENT} size="small" />
+  </div>
+);
+ListStepsSizes.story = { name: 'Tamaños' };
+
+export const ListStepsBorder = (): JSX.Element => (
+  <div className="storybook__wrapper-list-steps">
+    <ListSteps content={LISTSTEPS_CONTENT} size="large" isLight={true} />
+    <ListSteps content={LISTSTEPS_CONTENT} isLight={true} />
+    <ListSteps content={LISTSTEPS_CONTENT} size="small" isLight={true} />
+  </div>
+);
+ListStepsBorder.story = { name: 'Con borde' };
+
+export const ListStepsNumbers = (): JSX.Element => (
+  <div className="storybook__wrapper-list-steps">
+    <ListSteps content={LISTSTEPS_CONTENT} isNumbered={true} />
+    <ListSteps content={LISTSTEPS_CONTENT} isNumbered={true} isLight={true} />
+  </div>
+);
+ListStepsNumbers.story = { name: 'Con número' };
+
+//A futuro, tal vez se incluya la version de Pasos completados
+/* export const ListStepsComplete = (): JSX.Element => (
+  <div className="storybook__wrapper-list-steps">
+    <ListSteps content={LISTSTEPS_CONTENT_LIGHT_ITEMS} isNumbered={true} />
+  </div>
+);
+ListStepsComplete.story = { name: 'Con pasos completados' }; */
