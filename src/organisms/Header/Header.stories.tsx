6a36749f7c65e68e7c5327aca4a7b85d1735d28a
Victoria Nastasi <victorianastasi@gmail.com>
2023-09-18 16:21:56 -0300

    fix: header

--- src/organisms/Header/Header.stories.tsx
+++ src/organisms/Header/Header.stories.tsx
@@ -5,13 +5,16 @@ import './Header.stories.scss';
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
 
+// Components
+import { Dropdown, DropdownItem } from '../../components/Dropdown';
+
 // Configuración general del componente
 export default {
   title: 'Organismos|Header (encabezado)/Con buscador',
   decorators: [withA11y]
 };
 
-export const HeaderNoLogin = (): JSX.Element => {
+/* export const HeaderNoLogin = (): JSX.Element => {
   return (
     <div className="storybook__container-header">
       <header className="navbar navbar-light navbar-lg">
@@ -207,5 +210,424 @@ export const HeaderLogin = (): JSX.Element => {
 };
 
 HeaderLogin.story = {
+  name: 'Logueado'
+}; */
+
+export const SearchNoLogin = (): JSX.Element => {
+  return (
+    <div className="storybook__container-header">
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-search-btn"
+            data-toggle="collapse"
+            data-target="#navbarSearch1"
+            aria-controls="navbarSearch1"
+            aria-expanded="false"
+            aria-label="Buscador">
+            <i className="bx bx-search"></i>
+          </button>
+          <div className="navbar-user-btn">
+            <a
+              className="btn btn-lg btn-icon btn-outline-link"
+              href="#"
+              target="_blank"
+              aria-label="Accedé a tu cuenta">
+              <span className="material-icons-round">person</span>
+              <span className="btn-text-lg">Accedé a tu cuenta</span>
+              <span className="btn-text-sm">Acceder</span>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse nav-search" id="navbarSearch1">
+            <div className="nav nav-search-content">
+              <form className="form-search">
+                <div className="form-group">
+                  <input
+                    type="search"
+                    className="form-control input-search"
+                    id="name-input"
+                    name="name"
+                    placeholder="Buscar en Buenos Aires..."
+                  />
+                  <button className="reset" type="reset"></button>
+                  <button className="btn btn-secondary btn-icon btn-submit" type="submit">
+                    <span className="material-icons-round">east</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-search-btn"
+            data-toggle="collapse"
+            data-target="#navbarSearch2"
+            aria-controls="navbarSearch2"
+            aria-expanded="false"
+            aria-label="Buscador">
+            <i className="bx bx-search"></i>
+          </button>
+          <button
+            className="navbar-toggler collapsed navbar-sections-btn"
+            data-toggle="collapse"
+            data-target="#navbarSections"
+            aria-controls="navbarSections"
+            aria-expanded="false"
+            aria-label="Secciones">
+            <i className="bx bx-menu"></i>
+          </button>
+          <div className="navbar-user-btn">
+            <a
+              className="btn btn-lg btn-icon btn-outline-link"
+              href="#"
+              target="_blank"
+              aria-label="Accedé a tu cuenta">
+              <span className="material-icons-round">person</span>
+              <span className="btn-text-lg">Accedé a tu cuenta</span>
+              <span className="btn-text-sm">Acceder</span>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse nav-sections" id="navbarSections">
+            <ul className="nav nav-pills">
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg active">
+                  <span>Sección 1</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 2</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 3</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 4</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="collapse navbar-collapse nav-search" id="navbarSearch2">
+            <div className="nav nav-search-content">
+              <form className="form-search">
+                <div className="form-group">
+                  <input
+                    type="search"
+                    className="form-control input-search"
+                    id="name-input"
+                    name="name"
+                    placeholder="Buscar en Buenos Aires..."
+                  />
+                  <button className="reset" type="reset"></button>
+                  <button className="btn btn-secondary btn-icon btn-submit" type="submit">
+                    <span className="material-icons-round">east</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-search-btn"
+            data-toggle="collapse"
+            data-target="#navbarSearch3"
+            aria-controls="navbarSearch3"
+            aria-expanded="false"
+            aria-label="Buscador">
+            <i className="bx bx-search"></i>
+          </button>
+          <div className="navbar-sections-dropdown">
+            <Dropdown title="Menu" btnIconLeft="menu" isBordered isNested idDropdown="dropdownSections" isMenuRight>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+            </Dropdown>
+          </div>
+          <div className="navbar-user-btn">
+            <a
+              className="btn btn-lg btn-icon btn-outline-link"
+              href="#"
+              target="_blank"
+              aria-label="Accedé a tu cuenta">
+              <span className="material-icons-round">person</span>
+              <span className="btn-text-lg">Accedé a tu cuenta</span>
+              <span className="btn-text-sm">Acceder</span>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse nav-search" id="navbarSearch3">
+            <div className="nav nav-search-content">
+              <form className="form-search">
+                <div className="form-group">
+                  <input
+                    type="search"
+                    className="form-control input-search"
+                    id="name-input"
+                    name="name"
+                    placeholder="Buscar en Buenos Aires..."
+                  />
+                  <button className="reset" type="reset"></button>
+                  <button className="btn btn-secondary btn-icon btn-submit" type="submit">
+                    <span className="material-icons-round">east</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+SearchNoLogin.story = {
+  name: 'No logueado'
+};
+
+export const SearchLogin = (): JSX.Element => {
+  return (
+    <div className="storybook__container-header">
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-search-btn"
+            data-toggle="collapse"
+            data-target="#navbarSearch1"
+            aria-controls="navbarSearch1"
+            aria-expanded="false"
+            aria-label="Buscador">
+            <i className="bx bx-search"></i>
+          </button>
+          <div className="navbar-user-dropdown">
+            <Dropdown
+              title="Martina Pérez Leloir"
+              btnIconLeft="person"
+              isBordered
+              isNested
+              idDropdown="dropdownUser1"
+              isMenuRight>
+              <Dropdown
+                title="Opción en cascada"
+                isSubDropdown
+                btnIconLeft="expand_more"
+                btnIconLeftClass="btn-dropdown-icon"
+                isMenuRight>
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+              </Dropdown>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem itemIconRight="logout" isDanger />
+            </Dropdown>
+          </div>
+          <div className="collapse navbar-collapse nav-search" id="navbarSearch1">
+            <div className="nav nav-search-content">
+              <form className="form-search">
+                <div className="form-group">
+                  <input
+                    type="search"
+                    className="form-control input-search"
+                    id="name-input"
+                    name="name"
+                    placeholder="Buscar en Buenos Aires..."
+                  />
+                  <button className="reset" type="reset"></button>
+                  <button className="btn btn-secondary btn-icon btn-submit" type="submit">
+                    <span className="material-icons-round">east</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-search-btn"
+            data-toggle="collapse"
+            data-target="#navbarSearch2"
+            aria-controls="navbarSearch2"
+            aria-expanded="false"
+            aria-label="Buscador">
+            <i className="bx bx-search"></i>
+          </button>
+          <button
+            className="navbar-toggler collapsed navbar-sections-btn"
+            data-toggle="collapse"
+            data-target="#navbarSections"
+            aria-controls="navbarSections"
+            aria-expanded="false"
+            aria-label="Secciones">
+            <i className="bx bx-menu"></i>
+          </button>
+          <div className="navbar-user-dropdown">
+            <Dropdown
+              title="Martina Pérez Leloir"
+              btnIconLeft="person"
+              isBordered
+              isNested
+              idDropdown="dropdownUser2"
+              isMenuRight>
+              <Dropdown
+                title="Opción en cascada"
+                isSubDropdown
+                btnIconLeft="expand_more"
+                btnIconLeftClass="btn-dropdown-icon"
+                isMenuRight>
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+              </Dropdown>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem itemIconRight="logout" isDanger />
+            </Dropdown>
+          </div>
+          <div className="collapse navbar-collapse nav-sections" id="navbarSections">
+            <ul className="nav nav-pills">
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg active">
+                  <span>Sección 1</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 2</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 3</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 4</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="collapse navbar-collapse nav-search" id="navbarSearch2">
+            <div className="nav nav-search-content">
+              <form className="form-search">
+                <div className="form-group">
+                  <input
+                    type="search"
+                    className="form-control input-search"
+                    id="name-input"
+                    name="name"
+                    placeholder="Buscar en Buenos Aires..."
+                  />
+                  <button className="reset" type="reset"></button>
+                  <button className="btn btn-secondary btn-icon btn-submit" type="submit">
+                    <span className="material-icons-round">east</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-search-btn"
+            data-toggle="collapse"
+            data-target="#navbarSearch3"
+            aria-controls="navbarSearch3"
+            aria-expanded="false"
+            aria-label="Buscador">
+            <i className="bx bx-search"></i>
+          </button>
+          <div className="navbar-sections-dropdown">
+            <Dropdown title="Menu" btnIconLeft="menu" isBordered isNested idDropdown="dropdownSections" isMenuRight>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+            </Dropdown>
+          </div>
+          <div className="navbar-user-dropdown">
+            <Dropdown
+              title="Martina Pérez Leloir"
+              btnIconLeft="person"
+              isBordered
+              isNested
+              idDropdown="dropdownUser3"
+              isMenuRight>
+              <Dropdown
+                title="Opción en cascada"
+                isSubDropdown
+                btnIconLeft="expand_more"
+                btnIconLeftClass="btn-dropdown-icon"
+                isMenuRight>
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+              </Dropdown>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem itemIconRight="logout" isDanger />
+            </Dropdown>
+          </div>
+          <div className="collapse navbar-collapse nav-search" id="navbarSearch3">
+            <div className="nav nav-search-content">
+              <form className="form-search">
+                <div className="form-group">
+                  <input
+                    type="search"
+                    className="form-control input-search"
+                    id="name-input"
+                    name="name"
+                    placeholder="Buscar en Buenos Aires..."
+                  />
+                  <button className="reset" type="reset"></button>
+                  <button className="btn btn-secondary btn-icon btn-submit" type="submit">
+                    <span className="material-icons-round">east</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+SearchLogin.story = {
   name: 'Logueado'
 };
--- src/organisms/Header/HeaderWithoutSearch.stories.tsx
+++ src/organisms/Header/HeaderWithoutSearch.stories.tsx
@@ -5,13 +5,16 @@ import './Header.stories.scss';
 // Addons
 import { withA11y } from '@storybook/addon-a11y';
 
+// Components
+import { Dropdown, DropdownItem } from '../../components/Dropdown';
+
 // Configuración general del componente
 export default {
   title: 'Organismos|Header (encabezado)/Sin buscador',
   decorators: [withA11y]
 };
 
-export const HeaderNoLoginSearch = (): JSX.Element => {
+/* export const HeaderNoLoginSearch = (): JSX.Element => {
   return (
     <div className="storybook__container-header">
       <header className="navbar navbar-light navbar-md">
@@ -117,5 +120,256 @@ export const HeaderLoginSearch = (): JSX.Element => {
 };
 
 HeaderLoginSearch.story = {
+  name: 'Logueado'
+}; */
+
+export const NoLogin = (): JSX.Element => {
+  return (
+    <div className="storybook__container-header">
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <div className="navbar-user-btn">
+            <a
+              className="btn btn-lg btn-icon btn-outline-link"
+              href="#"
+              target="_blank"
+              aria-label="Accedé a tu cuenta">
+              <span className="material-icons-round">person</span>
+              <span className="btn-text-lg">Accedé a tu cuenta</span>
+              <span className="btn-text-sm">Acceder</span>
+            </a>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-sections-btn"
+            data-toggle="collapse"
+            data-target="#navbarSections"
+            aria-controls="navbarSections"
+            aria-expanded="false"
+            aria-label="Secciones">
+            <i className="bx bx-menu"></i>
+          </button>
+          <div className="navbar-user-btn">
+            <a
+              className="btn btn-lg btn-icon btn-outline-link"
+              href="#"
+              target="_blank"
+              aria-label="Accedé a tu cuenta">
+              <span className="material-icons-round">person</span>
+              <span className="btn-text-lg">Accedé a tu cuenta</span>
+              <span className="btn-text-sm">Acceder</span>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse nav-sections" id="navbarSections">
+            <ul className="nav nav-pills">
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg active">
+                  <span>Sección 1</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 2</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 3</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 4</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <div className="navbar-sections-dropdown">
+            <Dropdown title="Menu" btnIconLeft="menu" isBordered isNested idDropdown="dropdownSections1" isMenuRight>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+            </Dropdown>
+          </div>
+          <div className="navbar-user-btn">
+            <a
+              className="btn btn-lg btn-icon btn-outline-link"
+              href="#"
+              target="_blank"
+              aria-label="Accedé a tu cuenta">
+              <span className="material-icons-round">person</span>
+              <span className="btn-text-lg">Accedé a tu cuenta</span>
+              <span className="btn-text-sm">Acceder</span>
+            </a>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+NoLogin.story = {
+  name: 'No logueado'
+};
+
+export const Login = (): JSX.Element => {
+  return (
+    <div className="storybook__container-header">
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <div className="navbar-user-dropdown">
+            <Dropdown
+              title="Martina Pérez Leloir"
+              btnIconLeft="person"
+              isBordered
+              isNested
+              idDropdown="dropdownUser1"
+              isMenuRight>
+              <Dropdown
+                title="Opción en cascada"
+                isSubDropdown
+                btnIconLeft="expand_more"
+                btnIconLeftClass="btn-dropdown-icon"
+                isMenuRight>
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+              </Dropdown>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem itemIconRight="logout" isDanger />
+            </Dropdown>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <button
+            className="navbar-toggler collapsed navbar-sections-btn"
+            data-toggle="collapse"
+            data-target="#navbarSections"
+            aria-controls="navbarSections"
+            aria-expanded="false"
+            aria-label="Secciones">
+            <i className="bx bx-menu"></i>
+          </button>
+          <div className="navbar-user-dropdown">
+            <Dropdown
+              title="Martina Pérez Leloir"
+              btnIconLeft="person"
+              isBordered
+              isNested
+              idDropdown="dropdownUser2"
+              isMenuRight>
+              <Dropdown
+                title="Opción en cascada"
+                isSubDropdown
+                btnIconLeft="expand_more"
+                btnIconLeftClass="btn-dropdown-icon"
+                isMenuRight>
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+              </Dropdown>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem itemIconRight="logout" isDanger />
+            </Dropdown>
+          </div>
+          <div className="collapse navbar-collapse nav-sections" id="navbarSections">
+            <ul className="nav nav-pills">
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg active">
+                  <span>Sección 1</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 2</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 3</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-link-lg">
+                  <span>Sección 4</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <header className="navbar navbar-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img className="header-logo" src="header/logotipo_ba.svg" alt="Inicio" />
+          </a>
+          <div className="navbar-sections-dropdown">
+            <Dropdown title="Menu" btnIconLeft="menu" isBordered isNested idDropdown="dropdownSections" isMenuRight>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+            </Dropdown>
+          </div>
+          <div className="navbar-user-dropdown">
+            <Dropdown
+              title="Martina Pérez Leloir"
+              btnIconLeft="person"
+              isBordered
+              isNested
+              idDropdown="dropdownUser3"
+              isMenuRight>
+              <Dropdown
+                title="Opción en cascada"
+                isSubDropdown
+                btnIconLeft="expand_more"
+                btnIconLeftClass="btn-dropdown-icon"
+                isMenuRight>
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+                <DropdownItem isSubItem />
+              </Dropdown>
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem />
+              <DropdownItem itemIconRight="logout" isDanger />
+            </Dropdown>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+Login.story = {
   name: 'Logueado'
 };
