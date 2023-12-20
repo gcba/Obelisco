9c2f35151f28b256a63c9290d5b052712f135848
Victoria Nastasi <victorianastasi@gmail.com>
2023-12-20 09:30:55 -0300

    fix: se agrega clase text-truncate para titulo

--- src/components/Card/CardsVertical.stories.tsx
+++ src/components/Card/CardsVertical.stories.tsx
@@ -12,6 +12,8 @@ export default {
 };
 
 const title = 'Título de la tarjeta';
+const titleExtended =
+  'Título de la tarjeta Noticias truncado con texto extendido que supera las tres líneas de longitud.';
 const description = 'Descripción de la tarjeta';
 
 export const WithIcon = (): JSX.Element => {
@@ -75,6 +77,25 @@ export const News = (): JSX.Element => {
           <h3 className="card-title">
             <a href="#" className="card-title-link">
               {title}
+            </a>
+          </h3>
+          <p className="card-text">{description}</p>
+          <div className="card-info">
+            <small>Fecha de publicación</small>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="card">
+        <img src="cards/img-top.jpg" className="card-img-top" alt="descripción de imagen" />
+        <div className="card-body">
+          <div className="card-badges">
+            <span className="badge badge-secondary">Etiqueta 1</span>
+            <span className="badge badge-secondary">Etiqueta 2</span>
+          </div>
+          <h3 className="card-title">
+            <a href="#" className="card-title-link text-truncate">
+              {titleExtended}
             </a>
           </h3>
           <p className="card-text">{description}</p>
