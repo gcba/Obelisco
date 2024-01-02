912ef97bffdc0f49210d8a3b70348c7461a4d711
davisLjr <davisprojectjr@gmail.com>
2024-01-02 16:51:43 -0300

    fix: actualizacion de direccion

--- src/components/Mapa/Map.stories.tsx
+++ src/components/Mapa/Map.stories.tsx
@@ -15,7 +15,7 @@ export default {
 export const Map = (): JSX.Element => {
   const iframeElement = (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.611312241825!2d-58.47500842445462!3d-34.5633954553695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6878f2d4d9d%3A0x8553561751a33b4!2sAv.%20Monroe%203555%2C%20C1430BKC%20CABA!5e0!3m2!1ses-419!2sar!4v1702306415307!5m2!1ses-419!2sar"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.561313314447!2d-58.40988132452283!3d-34.64052465944252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb0de7f4d675%3A0xfc2ef5324296a64!2sUspallata%203160%2C%20C1437JCL%20CABA!5e0!3m2!1ses!2sar!4v1704218461718!5m2!1ses!2sar"
       width="600"
       height="146"
       title="Mapa de como llegar"
@@ -29,9 +29,9 @@ export const Map = (): JSX.Element => {
     <div className="storybook__container-card-map">
       <MapaCard
         title="Ubicación"
-        description="Av. Monroe 3555"
+        description="Av. Uspallata 3160"
         iframe={iframeElement}
-        link="https://mapa.buenosaires.gob.ar/comollego/?lat=-34.565106&lng=-58.470986&zl=15&modo=transporte&dir=Monroe+Av.+3555"
+        link="https://mapa.buenosaires.gob.ar/comollego/?lat=-34.640340&lng=-58.407032&zl=15&modo=transporte&dir=Uspallata+3160"
       />
     </div>
   );
