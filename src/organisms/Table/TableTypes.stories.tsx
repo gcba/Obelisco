5b93566a33cfc4d665bd874cfae79ae37a5680ad
davisLjr <davisprojectjr@gmail.com>
2024-02-22 13:24:18 -0300

    fix: se agrega variante con boton de icono

--- src/organisms/Table/TableTypes.stories.tsx
+++ src/organisms/Table/TableTypes.stories.tsx
@@ -375,3 +375,123 @@ export const TextAndButton = (): JSX.Element => {
 TextAndButton.story = {
   name: 'De botón'
 };
+
+export const TextAndButtonIcon = (): JSX.Element => {
+  return (
+    <div className="storybook__container-table-responsive">
+      <div className="responsive-scroll" tabIndex={0}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col" className="tb-text">
+                Texto
+              </th>
+              <th scope="col" className="tb-text">
+                Texto
+              </th>
+              <th scope="col" className="tb-text">
+                Texto
+              </th>
+              <th scope="col" className="tb-button">
+                Botón de ícono
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                Este un texto de prueba para registrar el alto máximo de la celda y la cantidad de caracteres
+                permitidos.
+              </td>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>
+                <div className="actions-table">
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">edit</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">visibility</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">file_download</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">delete</span>
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>
+                <div className="actions-table">
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">edit</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">visibility</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">file_download</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">delete</span>
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>
+                <div className="actions-table">
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">edit</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">visibility</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">file_download</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">delete</span>
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>Texto predeterminado</td>
+              <td>
+                <div className="actions-table">
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">edit</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">visibility</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">file_download</span>
+                  </button>
+                  <button className="button-actions-table">
+                    <span className="material-icons-round">delete</span>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+TextAndButtonIcon.story = {
+  name: 'De botón de ícono'
+};
