afba682aedeeebdb623284783b8161919c409d86
Lautaro Rodriguez <lar1996.vs@gmail.com>
2024-04-22 13:44:10 -0300

    style: se saca duplicado

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -25,16 +25,6 @@ export const Switch = (): JSX.Element => {
         <SwitchContainer label="Apagado" id="switch-dark-1" isDark />
         <SwitchContainer label="Prendido" id="switch-dark-2" isDark isChecked />
       </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado" id="switch-1" />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 label="Prendido" id="switch-2" isChecked />
-      </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado" id="switch-dark-1" isDark />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 label="Prendido" id="switch-dark-2" isDark isChecked />
-      </div>
     </>
   );
 };
@@ -52,16 +42,6 @@ export const SwitchWithoutLabel = (): JSX.Element => {
       <div className="storybook__container-switch">
         <SwitchContainer id="switch-label-hidden-1" isLabelHidden isDark />
         <SwitchContainer id="switch-label-hidden-2" isLabelHidden isDark isChecked />
-      </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado" id="switch-label-hidden-1" isLabelHidden />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 label="Prendido" id="switch-label-hidden-2" isLabelHidden isChecked />
-      </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado" id="switch-label-hidden-3" isLabelHidden isDark />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 label="Prendido" id="switch-label-hidden-4" isLabelHidden isChecked isDark />
       </div>
     </>
   );
@@ -81,16 +61,6 @@ export const SwitchSizes = (): JSX.Element => {
         <SwitchContainer label="Apagado" id="switch-small-1" isSmall isLabelHidden />
         <SwitchContainer label="Prendido" id="switch-small-2" isSmall isLabelHidden isChecked />
       </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado" id="switch-1" />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 label="Prendido" id="switch-2" isChecked />
-      </div>
-      <div className="mb-3">
-        <SwitchContainer2 id="switch-small-1" isSmall />
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 id="switch-small-2" isSmall isChecked />
-      </div>
     </>
   );
 };
@@ -107,23 +77,6 @@ export const SwitchDisabled = (): JSX.Element => {
       <div className="storybook__container-switch">
         <SwitchContainer label="Apagado / Deshabilitado" id="switch-disabled-1" isDisabled isLabelHidden />
         <SwitchContainer label="Prendido / Deshabilitado" id="switch-disabled-2" isDisabled isLabelHidden isChecked />
-      </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado / Deshabilitado" id="switch-1" isDisabled />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2 label="Prendido / Deshabilitado" id="switch-2" isChecked isDisabled />
-      </div>
-      <div className="mb-3">
-        <SwitchContainer2 label="Apagado / Deshabilitado" id="switch-disabled-1" isSmall isDisabled isLabelHidden />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <SwitchContainer2
-          label="Prendido / Deshabilitado"
-          id="switch-disabled-2"
-          isSmall
-          isChecked
-          isDisabled
-          isLabelHidden
-        />
       </div>
     </>
   );
