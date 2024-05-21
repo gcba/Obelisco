5e4172cee770708ab8e7910ebccf4ca36debda5d
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-21 12:06:02 -0300

    fix: Se cambia la forma de mostrar Titulares. Y se quita la hoja de Titulares que ya no se necesita

--- src/styles/Typography/TypographyHeadlines.stories.tsx
+++ src/styles/Typography/TypographyHeadlines.stories.tsx
@@ -12,83 +12,246 @@ export default {
   decorators: [withA11y]
 };
 
-export const TagL = (): JSX.Element => {
+export const Tag = (): JSX.Element => {
   return (
     <>
-      <Typography as="p" className="headline-lg">
-        Titular L: Familia tipográfica: Nunito | Tamaño: 20px | Interlineado: 24px.
-      </Typography>
-      <Typography as="p" className="headline-lg">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-lg" weight="font-weight-semibold">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-lg" weight="font-weight-bold">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-lg underline-1">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-lg underline-1" weight="font-weight-bold">
-        Esto es un texto de titulares
-      </Typography>
+      {/* Titulares L */}
+      <p className="headline-lgg font-weight-bold">Titulares L</p>
+      <div className="responsive-scroll" tabIndex={0}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col" className="tb-text">
+                HTML
+              </th>
+              <th scope="col" className="tb-text">
+                Familia Tipografica | Tamaño | Interlineado
+              </th>
+              <th scope="col" className="tb-text">
+                Ejemplos
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-lg&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 20px | 24px </td>
+              <td>
+                <Typography as="p" className="headline-lg ">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-lg font-weight-semibold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 28px | semibold</td>
+              <td>
+                <Typography as="p" className="headline-lg font-weight-semibold">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-lg font-weight-bold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 28px | bold </td>
+              <td>
+                <Typography as="p" className="headline-lg font-weight-bold">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-lg font-italic&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Open Sans | 18px | 28px | italic </td>
+              <td>
+                <Typography as="p" className="headline-lg font-italic">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-lg underline-1&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Open Sans | 18px | 28px | underline </td>
+              <td>
+                <Typography as="p" className="headline-lg underline-1">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {/* Titulares L */}
+
+      <hr />
+      {/* Titulares M */}
+      <p className="headline-lgg font-weight-bold">Titulares M</p>
+      <div className="responsive-scroll" tabIndex={0}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col" className="tb-text">
+                HTML
+              </th>
+              <th scope="col" className="tb-text">
+                Familia Tipografica | Tamaño | Interlineado
+              </th>
+              <th scope="col" className="tb-text">
+                Ejemplos
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-md&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 24px </td>
+              <td>
+                <Typography as="p" className="headline-md ">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-md font-weight-semibold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 24px | semibold</td>
+              <td>
+                <Typography as="p" className="headline-md font-weight-semibold">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-md font-weight-bold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 24px | bold </td>
+              <td>
+                <Typography as="p" className="headline-md font-weight-bold">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-md font-italic&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 24px | italic </td>
+              <td>
+                <Typography as="p" className="headline-md font-italic">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;headline-md underline-1&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 18px | 24px | underline </td>
+              <td>
+                <Typography as="p" className="headline-md underline-1">
+                  Esto es un texto de titulares
+                </Typography>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {/* Titulares M */}
+
+      <hr />
+      {/* Titulares S */}
+      <p className="headline-lgg font-weight-bold">Titulares S</p>
+      <div className="responsive-scroll" tabIndex={0}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col" className="tb-text">
+                HTML
+              </th>
+              <th scope="col" className="tb-text">
+                Familia Tipografica | Tamaño | Interlineado
+              </th>
+              <th scope="col" className="tb-text">
+                Ejemplos
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-sm&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 16px | 20px </td>
+              <td>
+                <Typography as="p" className="text-sm ">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-sm font-weight-semibold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 16px | 20px | semibold</td>
+              <td>
+                <Typography as="p" className="text-sm font-weight-semibold">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-sm font-weight-bold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 16px | 20px | bold </td>
+              <td>
+                <Typography as="p" className="text-sm font-weight-bold">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-sm font-italic&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 16px | 20px | italic </td>
+              <td>
+                <Typography as="p" className="text-sm font-italic">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-sm underline-1&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Nunito | 16px | 20px | underline </td>
+              <td>
+                <Typography as="p" className="text-sm underline-1">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {/* Titulares S */}
     </>
   );
 };
 
-TagL.story = { name: 'Titulares L' };
-
-export const TagM = (): JSX.Element => {
-  return (
-    <>
-      <Typography as="p" className="headline-md">
-        Titular M: Familia tipográfica: Nunito | Tamaño: 18px | Interlineado: 24px.
-      </Typography>
-      <Typography as="p" className="headline-md">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-md" weight="font-weight-semibold">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-md" weight="font-weight-bold">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-md underline-1">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-md underline-1" weight="font-weight-bold">
-        Esto es un texto de titulares
-      </Typography>
-    </>
-  );
-};
-
-TagM.story = { name: 'Titulares M' };
-
-export const TagS = (): JSX.Element => {
-  return (
-    <>
-      <Typography as="p" className="headline-sm">
-        Titular M: Familia tipográfica: Nunito | Tamaño: 16px | Interlineado: 20px.
-      </Typography>
-      <Typography as="p" className="headline-sm">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-sm" weight="font-weight-semibold">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-sm" weight="font-weight-bold">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-sm underline-1">
-        Esto es un texto de titulares
-      </Typography>
-      <Typography as="p" className="headline-sm underline-1" weight="font-weight-bold">
-        Esto es un texto de titulares
-      </Typography>
-    </>
-  );
-};
-
-TagS.story = { name: 'Titulares S' };
+Tag.story = { name: 'Titulares' };
