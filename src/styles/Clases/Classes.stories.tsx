e62a83f6b52f8351557648ce11d93b5eebba5765
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-24 10:11:16 -0300

    feat: Se quita el panel de HTML en 'Clases'

--- src/styles/Clases/Classes.stories.tsx
+++ src/styles/Clases/Classes.stories.tsx
@@ -1,51 +1,112 @@
 // Base
 import React from 'react';
 import './Classes.stories.scss';
-// Addons
-import { withA11y } from '@storybook/addon-a11y';
+import { Typography } from '../Typography/index';
 
 // Configuración general del componente
 export default {
   title: 'Estilos|Clases',
-  decorators: [withA11y]
+  parameters: { options: { showPanel: false } }
 };
 
 export const SizesAndSpacing = (): JSX.Element => {
   return (
-    <div className="storybook__container-grid">
-      <div className="container"></div>
-    </div>
+    <>
+      <p className="headline-lg font-weight-bold">Tamaños y Espaciados</p>
+      <div className="responsive-scroll" tabIndex={0}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col" className="tb-text">
+                CLASES
+              </th>
+              <th scope="col" className="tb-text">
+                Propiedad que aplica
+              </th>
+              <th scope="col" className="tb-text">
+                Ejemplos
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p>{'.mt-0'}</p>
+              </td>
+              <td> {'margin-top: 0;'} </td>
+              <td>
+                <Typography as="p" className="mb-0 text-xl ">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-xl font-weight-semibold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Open Sans | 20px | 28px | semibold</td>
+              <td>
+                <Typography as="p" className="mb-0 text-xl font-weight-semibold">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-xl font-weight-bold&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Open Sans | 20px | 28px | bold </td>
+              <td>
+                <Typography as="p" className="mb-0 text-xl font-weight-bold">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-xl font-italic&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Open Sans | 20px | 28px | italic </td>
+              <td>
+                <Typography as="p" className="mb-0 text-xl font-italic">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>&lt;p class=&quot;text-xl underline-1&quot;&gt;&lt;/p&gt;</p>
+              </td>
+              <td> Open Sans | 20px | 28px | underline </td>
+              <td>
+                <Typography as="p" className="mb-0 text-xl underline-1">
+                  Este es un párrafo de Obelisco, el sistema de diseño del Gobierno de la Ciudad de Buenos Aires.
+                </Typography>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
 SizesAndSpacing.story = { name: 'Tamaños y Espaciados' };
 
 export const Columns = (): JSX.Element => {
-  return (
-    <div className="storybook__container-grid">
-      <div className="container"></div>
-    </div>
-  );
+  return <></>;
 };
 
 Columns.story = { name: 'Columnas' };
 
 export const AlignAndOrder = (): JSX.Element => {
-  return (
-    <div className="storybook__container-grid">
-      <div className="container"></div>
-    </div>
-  );
+  return <></>;
 };
 
 AlignAndOrder.story = { name: 'Align y Order' };
 
 export const Border = (): JSX.Element => {
-  return (
-    <div className="storybook__container-grid">
-      <div className="container"></div>
-    </div>
-  );
+  return <></>;
 };
 
 Border.story = { name: 'Bordes' };
