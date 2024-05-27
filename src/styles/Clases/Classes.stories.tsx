efaee00919006086723c0cd58124bb7b2a8f11f7
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-27 11:45:17 -0300

    docs: Se aplica la clase storybook__margin-typography en los p de las tablas

--- src/styles/Clases/Classes.stories.tsx
+++ src/styles/Clases/Classes.stories.tsx
@@ -35,128 +35,128 @@ export const SizesAndSpacing = (): JSX.Element => {
           <tbody>
             <tr>
               <td>
-                <p className="mt-0">{'.mt-0'}</p>
+                <p className="storybook__margin-typography mt-0">{'.mt-0'}</p>
               </td>
               <td>
-                <p className="mr-0">{'.mr-0'}</p>
+                <p className="storybook__margin-typography mr-0">{'.mr-0'}</p>
               </td>
               <td>
-                <p className="mb-0">{'.mb-0'}</p>
+                <p className="storybook__margin-typography mb-0">{'.mb-0'}</p>
               </td>
               <td>
-                <p className="ml-0">{'.ml-0'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="mt-1">{'.mt-1'}</p>
-              </td>
-              <td>
-                <p className="mr-1">{'.mr-1'}</p>
-              </td>
-              <td>
-                <p className="mb-1">{'.mb-1'}</p>
-              </td>
-              <td>
-                <p className="ml-1">{'.ml-1'}</p>
+                <p className="storybook__margin-typography ml-0">{'.ml-0'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="mt-2">{'.mt-2'}</p>
+                <p className="storybook__margin-typography mt-1">{'.mt-1'}</p>
               </td>
               <td>
-                <p className="mr-2">{'.mr-2'}</p>
+                <p className="storybook__margin-typography mr-1">{'.mr-1'}</p>
               </td>
               <td>
-                <p className="mb-2">{'.mb-2'}</p>
+                <p className="storybook__margin-typography mb-1">{'.mb-1'}</p>
               </td>
               <td>
-                <p className="ml-2">{'.ml-2'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="mt-3">{'.mt-3'}</p>
-              </td>
-              <td>
-                <p className="mr-3">{'.mr-3'}</p>
-              </td>
-              <td>
-                <p className="mb-3">{'.mb-3'}</p>
-              </td>
-              <td>
-                <p className="ml-3">{'.ml-3'}</p>
+                <p className="storybook__margin-typography ml-1">{'.ml-1'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="mt-4">{'.mt-4'}</p>
+                <p className="storybook__margin-typography mt-2">{'.mt-2'}</p>
               </td>
               <td>
-                <p className="mr-4">{'.mr-4'}</p>
+                <p className="storybook__margin-typography mr-2">{'.mr-2'}</p>
               </td>
               <td>
-                <p className="mb-4">{'.mb-4'}</p>
+                <p className="storybook__margin-typography mb-2">{'.mb-2'}</p>
               </td>
               <td>
-                <p className="ml-4">{'.ml-4'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="mt-5">{'.mt-5'}</p>
-              </td>
-              <td>
-                <p className="mr-5">{'.mr-5'}</p>
-              </td>
-              <td>
-                <p className="mb-5">{'.mb-5'}</p>
-              </td>
-              <td>
-                <p className="ml-5">{'.ml-5'}</p>
+                <p className="storybook__margin-typography ml-2">{'.ml-2'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="mt-6">{'.mt-6'}</p>
+                <p className="storybook__margin-typography mt-3">{'.mt-3'}</p>
               </td>
               <td>
-                <p className="mr-6">{'.mr-6'}</p>
+                <p className="storybook__margin-typography mr-3">{'.mr-3'}</p>
               </td>
               <td>
-                <p className="mb-6">{'.mb-6'}</p>
+                <p className="storybook__margin-typography mb-3">{'.mb-3'}</p>
               </td>
               <td>
-                <p className="ml-6">{'.ml-6'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="mt-7">{'.mt-7'}</p>
-              </td>
-              <td>
-                <p className="mr-7">{'.mr-7'}</p>
-              </td>
-              <td>
-                <p className="mb-7">{'.mb-7'}</p>
-              </td>
-              <td>
-                <p className="ml-7">{'.ml-7'}</p>
+                <p className="storybook__margin-typography ml-3">{'.ml-3'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="mt-8">{'.mt-8'}</p>
+                <p className="storybook__margin-typography mt-4">{'.mt-4'}</p>
               </td>
               <td>
-                <p className="mr-8">{'.mr-8'}</p>
+                <p className="storybook__margin-typography mr-4">{'.mr-4'}</p>
               </td>
               <td>
-                <p className="mb-8">{'.mb-8'}</p>
+                <p className="storybook__margin-typography mb-4">{'.mb-4'}</p>
               </td>
               <td>
-                <p className="ml-8">{'.ml-8'}</p>
+                <p className="storybook__margin-typography ml-4">{'.ml-4'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography mt-5">{'.mt-5'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mr-5">{'.mr-5'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mb-5">{'.mb-5'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography ml-5">{'.ml-5'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography mt-6">{'.mt-6'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mr-6">{'.mr-6'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mb-6">{'.mb-6'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography ml-6">{'.ml-6'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography mt-7">{'.mt-7'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mr-7">{'.mr-7'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mb-7">{'.mb-7'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography ml-7">{'.ml-7'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography mt-8">{'.mt-8'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mr-8">{'.mr-8'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography mb-8">{'.mb-8'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography ml-8">{'.ml-8'}</p>
               </td>
             </tr>
           </tbody>
@@ -187,128 +187,128 @@ export const SizesAndSpacing = (): JSX.Element => {
           <tbody>
             <tr>
               <td>
-                <p className="pt-0">{'.pt-0'}</p>
+                <p className="storybook__margin-typography pt-0">{'.pt-0'}</p>
               </td>
               <td>
-                <p className="pr-0">{'.pr-0'}</p>
+                <p className="storybook__margin-typography pr-0">{'.pr-0'}</p>
               </td>
               <td>
-                <p className="pb-0">{'.pb-0'}</p>
+                <p className="storybook__margin-typography pb-0">{'.pb-0'}</p>
               </td>
               <td>
-                <p className="pl-0">{'.pl-0'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="pt-1">{'.pt-1'}</p>
-              </td>
-              <td>
-                <p className="pr-1">{'.pr-1'}</p>
-              </td>
-              <td>
-                <p className="pb-1">{'.pb-1'}</p>
-              </td>
-              <td>
-                <p className="pl-1">{'.pl-1'}</p>
+                <p className="storybook__margin-typography pl-0">{'.pl-0'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="pt-2">{'.pt-2'}</p>
+                <p className="storybook__margin-typography pt-1">{'.pt-1'}</p>
               </td>
               <td>
-                <p className="pr-2">{'.pr-2'}</p>
+                <p className="storybook__margin-typography pr-1">{'.pr-1'}</p>
               </td>
               <td>
-                <p className="pb-2">{'.pb-2'}</p>
+                <p className="storybook__margin-typography pb-1">{'.pb-1'}</p>
               </td>
               <td>
-                <p className="pl-2">{'.pl-2'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="pt-3">{'.pt-3'}</p>
-              </td>
-              <td>
-                <p className="pr-3">{'.pr-3'}</p>
-              </td>
-              <td>
-                <p className="pb-3">{'.pb-3'}</p>
-              </td>
-              <td>
-                <p className="pl-3">{'.pl-3'}</p>
+                <p className="storybook__margin-typography pl-1">{'.pl-1'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="pt-4">{'.pt-4'}</p>
+                <p className="storybook__margin-typography pt-2">{'.pt-2'}</p>
               </td>
               <td>
-                <p className="pr-4">{'.pr-4'}</p>
+                <p className="storybook__margin-typography pr-2">{'.pr-2'}</p>
               </td>
               <td>
-                <p className="pb-4">{'.pb-4'}</p>
+                <p className="storybook__margin-typography pb-2">{'.pb-2'}</p>
               </td>
               <td>
-                <p className="pl-4">{'.pl-4'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="pt-5">{'.pt-5'}</p>
-              </td>
-              <td>
-                <p className="pr-5">{'.pr-5'}</p>
-              </td>
-              <td>
-                <p className="pb-5">{'.pb-5'}</p>
-              </td>
-              <td>
-                <p className="pl-5">{'.pl-5'}</p>
+                <p className="storybook__margin-typography pl-2">{'.pl-2'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="pt-6">{'.pt-6'}</p>
+                <p className="storybook__margin-typography pt-3">{'.pt-3'}</p>
               </td>
               <td>
-                <p className="pr-6">{'.pr-6'}</p>
+                <p className="storybook__margin-typography pr-3">{'.pr-3'}</p>
               </td>
               <td>
-                <p className="pb-6">{'.pb-6'}</p>
+                <p className="storybook__margin-typography pb-3">{'.pb-3'}</p>
               </td>
               <td>
-                <p className="pl-6">{'.pl-6'}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="pt-7">{'.pt-7'}</p>
-              </td>
-              <td>
-                <p className="pr-7">{'.pr-7'}</p>
-              </td>
-              <td>
-                <p className="pb-7">{'.pb-7'}</p>
-              </td>
-              <td>
-                <p className="pl-7">{'.pl-7'}</p>
+                <p className="storybook__margin-typography pl-3">{'.pl-3'}</p>
               </td>
             </tr>
             <tr>
               <td>
-                <p className="pt-8">{'.pt-8'}</p>
+                <p className="storybook__margin-typography pt-4">{'.pt-4'}</p>
               </td>
               <td>
-                <p className="pr-8">{'.pr-8'}</p>
+                <p className="storybook__margin-typography pr-4">{'.pr-4'}</p>
               </td>
               <td>
-                <p className="pb-8">{'.pb-8'}</p>
+                <p className="storybook__margin-typography pb-4">{'.pb-4'}</p>
               </td>
               <td>
-                <p className="pl-8">{'.pl-8'}</p>
+                <p className="storybook__margin-typography pl-4">{'.pl-4'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography pt-5">{'.pt-5'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pr-5">{'.pr-5'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pb-5">{'.pb-5'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pl-5">{'.pl-5'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography pt-6">{'.pt-6'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pr-6">{'.pr-6'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pb-6">{'.pb-6'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pl-6">{'.pl-6'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography pt-7">{'.pt-7'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pr-7">{'.pr-7'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pb-7">{'.pb-7'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pl-7">{'.pl-7'}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p className="storybook__margin-typography pt-8">{'.pt-8'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pr-8">{'.pr-8'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pb-8">{'.pb-8'}</p>
+              </td>
+              <td>
+                <p className="storybook__margin-typography pl-8">{'.pl-8'}</p>
               </td>
             </tr>
           </tbody>
