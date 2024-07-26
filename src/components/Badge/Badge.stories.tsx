e0ce73d3c4cd359d0dc15f3defd747eff8f4c661
Victoria Nastasi <victorianastasi@gmail.com>
2024-07-26 11:42:22 -0300

    fix: etiquetas, alertas, acceso color de texto

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -30,11 +30,21 @@ Multiple.story = {
 
 export const Colores = (): JSX.Element => (
   <>
-    <Badge text="Etiqueta 1" className="badge-secondary" />
-    <Badge text="Etiqueta 2" className="badge-danger" />
-    <Badge text="Etiqueta 3" className="badge-success" />
-    <Badge text="Etiqueta 4" className="badge-info" />
-    <Badge text="Etiqueta 5" className="badge-primary" />
+    <div>
+      <Badge text="Etiqueta 1" className="badge-secondary" />
+      <Badge text="Etiqueta 2" className="badge-danger" />
+      <Badge text="Etiqueta 3" className="badge-success" />
+      <Badge text="Etiqueta 4" className="badge-info" />
+      <Badge text="Etiqueta 5" className="badge-primary" />
+    </div>
+    <div className="mt-5">
+      <Badge text="Secondary" className="badge-secondary" />
+      <Badge text="danger" className="badge-danger" />
+      <Badge text="success" className="badge-success" />
+      <Badge text="info" className="badge-info" />
+      <Badge text="warning" className="badge-warning" />
+      <Badge text="primary" className="badge-primary" />
+    </div>
   </>
 );
 
@@ -42,7 +52,44 @@ export const Link = (): JSX.Element => (
   <>
     <Badge text="Etiqueta 1" href="#" className="badge-secondary" />
     <Badge text="Etiqueta 2" href="#" className="hover badge-secondary" />
-    <Badge text="Etiqueta 3" href="#" className="focus hover badge-secondary" />
     <Badge text="Etiqueta 4" href="#" className="focus badge-secondary" />
+    <Badge text="Etiqueta 3" href="#" className="focus hover badge-secondary" />
+    <p className="headline-lg mt-3">Ejemplos de etiquetas link en distintos colores</p>
+    <div className="my-4">
+      <Badge text="Secondary" href="#" className="badge-secondary" />
+      <Badge text="Secondary" href="#" className="hover badge-secondary" />
+      <Badge text="Secondary" href="#" className="focus badge-secondary" />
+      <Badge text="Secondary" href="#" className="focus hover badge-secondary" />
+    </div>
+    <div className="mb-4">
+      <Badge text="danger" href="#" className="badge-danger" />
+      <Badge text="danger" href="#" className="hover badge-danger" />
+      <Badge text="danger" href="#" className="focus badge-danger" />
+      <Badge text="danger" href="#" className="focus hover badge-danger" />
+    </div>
+    <div className="mb-4">
+      <Badge text="success" href="#" className="badge-success" />
+      <Badge text="success" href="#" className="hover badge-success" />
+      <Badge text="success" href="#" className="focus badge-success" />
+      <Badge text="success" href="#" className="focus hover badge-success" />
+    </div>
+    <div className="mb-4">
+      <Badge text="info" href="#" className="badge-info" />
+      <Badge text="info" href="#" className="hover badge-info" />
+      <Badge text="info" href="#" className="focus badge-info" />
+      <Badge text="info" href="#" className="focus hover badge-info" />
+    </div>
+    <div className="mb-4">
+      <Badge text="warning" href="#" className="badge-warning" />
+      <Badge text="warning" href="#" className="hover badge-warning" />
+      <Badge text="warning" href="#" className="focus badge-warning" />
+      <Badge text="warning" href="#" className="focus hover badge-warning" />
+    </div>
+    <div className="mb-4">
+      <Badge text="primary" href="#" className="badge-primary" />
+      <Badge text="primary" href="#" className="hover badge-primary" />
+      <Badge text="primary" href="#" className="focus badge-primary" />
+      <Badge text="primary" href="#" className="focus hover badge-primary" />
+    </div>
   </>
 );
